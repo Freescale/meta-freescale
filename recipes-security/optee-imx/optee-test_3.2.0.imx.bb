Freescale/meta-freescale

commit 020d81896bba5f6cb1dae0f352854ce4d57374e8

optee-[client,test]: upgrade to 4.19.35_1.1.0
This upgrade is required to support new i.MX8M Nano SoC and is also
aligned with all optee-imx components.

Signed-off-by: Andrey Zhizhikin <andrey.zhizhikin@leica-geosystems.com>

recipes-security/optee-imx/optee-test_3.2.0.imx.bb
@@ -11,7 +11,7 @@ inherit pythonnative
 
 FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
 
-SRCBRANCH = "imx_4.14.78_1.0.0_ga"
+SRCBRANCH = "imx_4.19.35_1.1.0"
 OPTEE_TEST_SRC ?= "git://source.codeaurora.org/external/imx/imx-optee-test.git;protocol=https"
 
 SRC_URI = "${OPTEE_TEST_SRC};branch=${SRCBRANCH} \
@@ -21,7 +21,7 @@ SRC_URI = "${OPTEE_TEST_SRC};branch=${SRCBRANCH} \
 
 S = "${WORKDIR}/git"
 
-SRCREV = "eb7f698da9a7fa1587f96aa92ad8668abb0f0f48" 
+SRCREV = "b7114b828b82f2c0eec124ed424eff1230cc5319"
 
 
 

recipes-security/optee-imx/optee-client_3.2.0.imx.bb
@@ -7,11 +7,11 @@ LIC_FILES_CHKSUM = "file://LICENSE;md5=69663ab153298557a59c67a60a743e5b"
 
 inherit pythonnative systemd
 
-SRCBRANCH = "imx_4.14.78_1.0.0_ga"
+SRCBRANCH = "imx_4.19.35_1.1.0"
 OPTEE_CLIENT_SRC ?= "git://source.codeaurora.org/external/imx/imx-optee-client.git;protocol=https"
 SRC_URI = "${OPTEE_CLIENT_SRC};branch=${SRCBRANCH}"
 
-SRCREV = "d06647d201520ac57f1331e97db6138d63bc2666" 
+SRCREV = "71a9bef78fff2d5d4db8a2307d3b91e2aa671dc9"
 
 FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
 

commit b521e933e2b1539c58a4bd730b73dfbe4258e460

optee-test_3.2.0.imx: remove no longer required gcc fixes
These are now present in the new op-tee fork version.

Fixes: 020d818
Signed-off-by: Peter Griffin <peter.griffin@linaro.org>

recipes-security/optee-imx/optee-test_3.2.0.imx.bb
@@ -15,8 +15,6 @@ SRCBRANCH = "imx_4.19.35_1.1.0"
 OPTEE_TEST_SRC ?= "git://source.codeaurora.org/external/imx/imx-optee-test.git;protocol=https"
 
 SRC_URI = "${OPTEE_TEST_SRC};branch=${SRCBRANCH} \
-           file://0001-regression-4011-correct-potential-overflow.patch \
-           file://0001-xtest-prevent-unexpected-build-warning-with-strncpy.patch \
            file://0003-sock_server-fix-compilation-against-musl-sys-errno.h.patch \
            file://0004-build-ignore-declaration-after-statement-warnings.patch \
            file://0005-benchmark_1000-fix-compilation-against-musl-uint.patch \

recipes-security/optee-imx/optee-test/0001-regression-4011-correct-potential-overflow.patch
@@ -1,72 +1,0 @@
-Upstream-Status: Backport 3.4.0
-
-Signed-off-by: Peter Griffin <peter.griffin@linaro.org>
----
-From 0953bf0abb08fb98d24b7966001171a707fbb9b9 Mon Sep 17 00:00:00 2001
-From: Etienne Carriere <etienne.carriere@linaro.org>
-Date: Fri, 21 Dec 2018 15:36:25 +0100
-Subject: [PATCH] regression 4011: correct potential overflow
-MIME-Version: 1.0
-Content-Type: text/plain; charset=UTF-8
-Content-Transfer-Encoding: 8bit
-
-Fix issues reported by GCC 8.2.0.
-
-build/optee_test/host/xtest/regression_4000.c: In function ‘xtest_tee_test_4011’:
-build/optee_test/host/xtest/regression_4000.c:5029:3: error: ‘memmove’ pointer overflow between offset [0, 8] and size [4294967295, 2147483647] accessing array ‘tmp’ with type ‘uint8_t[1024]’ {aka ‘unsigned char[1024]’} [-Werror=array-bounds]
-   memmove(tmp + n + i, tmp + m, tmp_size - m);
-   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-build/optee_test/host/xtest/regression_4000.c:4927:10: note: array ‘tmp’ declared here
-  uint8_t tmp[1024];
-          ^~~
-build/optee_test/host/xtest/regression_4000.c:5029:3: error: ‘memmove’ specified size 4294967295 exceeds maximum object size 2147483647 [-Werror=stringop-overflow=]
-   memmove(tmp + n + i, tmp + m, tmp_size - m);
-   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-cc1: all warnings being treated as errors
-
-Reported-by: Simon Hughes <simon.hughes@arm.com>
-Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
-Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
----
- host/xtest/regression_4000.c | 16 +++++++++++++---
- 1 file changed, 13 insertions(+), 3 deletions(-)
-
-diff --git a/host/xtest/regression_4000.c b/host/xtest/regression_4000.c
-index 766aad2..205a226 100644
---- a/host/xtest/regression_4000.c
-+++ b/host/xtest/regression_4000.c
-@@ -5018,18 +5018,28 @@ static void xtest_tee_test_4011(ADBG_Case_t *c)
- 				out, out_size, tmp, &tmp_size)))
- 			goto out;
- 
-+		if (!ADBG_EXPECT_COMPARE_UNSIGNED(c, tmp_size, <=, sizeof(tmp)))
-+			goto out;
-+
- 		/* 4.1 */
--		for (n = 0; n < tmp_size; n++)
-+		for (n = 0; n < tmp_size - i; n++)
- 			if (tmp[n] == 0xff)
- 				break;
-+
-+		/* Shall find at least a padding start before buffer end */
-+	        if (!ADBG_EXPECT_COMPARE_UNSIGNED(c, n, <, tmp_size - i - 1))
-+			goto out;
-+
- 		for (m = n + 1; m < tmp_size; m++)
- 			if (tmp[m] != 0xff)
- 				break;
-+
- 		/* 4.2 */
- 		memmove(tmp + n + i, tmp + m, tmp_size - m);
-+
- 		/* 4.3 */
--		for (n = n + tmp_size - m + i; n < tmp_size; n++)
--			tmp[n] = 0;
-+		n = n + i + tmp_size - m;
-+		memset(tmp + n, 0, tmp_size - n);
- 
- 		/* 5 */
- 		out_size = sizeof(out);
--- 
-2.7.4
-

recipes-security/optee-imx/optee-test/0001-xtest-prevent-unexpected-build-warning-with-strncpy.patch
@@ -1,66 +1,0 @@
-Upstream-Status: Backport 3.4.0
-
-Signed-off-by: Peter Griffin <peter.griffin@linaro.org>
----
-From 493574ad1f4f56dd63097a652b87c25c507ce99c Mon Sep 17 00:00:00 2001
-From: Etienne Carriere <etienne.carriere@linaro.org>
-Date: Fri, 21 Dec 2018 15:36:00 +0100
-Subject: [PATCH] xtest: prevent unexpected build warning with strncpy
-MIME-Version: 1.0
-Content-Type: text/plain; charset=UTF-8
-Content-Transfer-Encoding: 8bit
-
-This change modifies adbg_run.c to prevent a false positive
-warning reported by GCC 8.2 on usage of strncpy():
-
-    build/optee_test/host/xtest/adbg/src/adbg_run.c: In function ‘Do_ADBG_AppendToSuite’:
-    build/optee_test/host/xtest/adbg/src/adbg_run.c:103:3: error: ‘strncpy’ specified bound depends on the length of the source argument [-Werror=stringop-overflow=]
-       strncpy(p, Source_p->SuiteID_p, size);
-       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    build/optee_test/host/xtest/adbg/src/adbg_run.c:88:9: note: length computed here
-      size = strlen(Source_p->SuiteID_p);
-             ^~~~~~~~~~~~~~~~~~~~~~~~~~~
-    cc1: all warnings being treated as errors
-
-From [1]:
-  Using strncpy Safely
-  In general, it is not possible to avoid string truncation by strncpy
-  except by sizing the destination to be at least a byte larger than
-  the length of the source string. With that approach, however, using
-  strncpy becomes unnecessary and the function can be avoided in favor
-  of other APIs such as strcpy or (less preferably) memcpy. Much has
-  been written about the problems with strncpy and we recommend to
-  avoid it whenever possible. It is, however, worth keeping in mind
-  that unlike other standard string-handling functions, strncpy always
-  writes exactly as many characters as specified by the third argument;
-  if the source string is shorter, the function fills the remaining
-  bytes with NULs.
-
-This change prefers using a snprintf() as used in the alternate
-instruction block of the strncpy() call.
-
-[1] https://developers.redhat.com/blog/2018/05/24/detecting-string-truncation-with-gcc-8/
-
-Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
-Signed-off-by: Simon Hughes <simon.hughes@arm.com>
-Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
----
- host/xtest/adbg/src/adbg_run.c | 2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
-
-diff --git a/host/xtest/adbg/src/adbg_run.c b/host/xtest/adbg/src/adbg_run.c
-index 406e429..2739db5 100644
---- a/host/xtest/adbg/src/adbg_run.c
-+++ b/host/xtest/adbg/src/adbg_run.c
-@@ -100,7 +100,7 @@ int Do_ADBG_AppendToSuite(
- 		snprintf(p, size, "%s+%s", Dest_p->SuiteID_p,
- 			 Source_p->SuiteID_p);
- 	else
--		strncpy(p, Source_p->SuiteID_p, size);
-+		snprintf(p, size, "%s", Source_p->SuiteID_p);
- 	free((void *)Dest_p->SuiteID_p);
- 	Dest_p->SuiteID_p = p;
- 
--- 
-2.7.4
-