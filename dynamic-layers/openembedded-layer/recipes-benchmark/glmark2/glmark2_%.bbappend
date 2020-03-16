Freescale/meta-freescale

commit 7801868f4969bc3f93f9d419e7852206e45fad53

glmark2: Remove bbappend
The default PACKAGECONFIG for glmark2 builds just fine on imxgpu3d and
imxgpu2d platforms, and doing so enables support for the drm based
tests.

Tested on imx8mq and imx8qm

Signed-off-by: Joshua Watt <Joshua.Watt@garmin.com>

dynamic-layers/openembedded-layer/recipes-benchmark/glmark2/glmark2_%.bbappend
@@ -1,4 +1,0 @@
-PACKAGECONFIG_imxgpu3d = "${@bb.utils.contains('DISTRO_FEATURES', 'wayland opengl', 'wayland-gles2', \
-                                bb.utils.contains('DISTRO_FEATURES', 'x11 opengl',     'x11-gl x11-gles2', '', d), d)}"
-PACKAGECONFIG_imxgpu2d = "${@bb.utils.contains('DISTRO_FEATURES', 'wayland opengl', '', \
-                                bb.utils.contains('DISTRO_FEATURES', 'x11 opengl',     'x11-gl', '', d), d)}"

commit 32ba6ddb613230bfb899d9a20c3a83cfe7c04842

glmark2: don't build drm flavours for machines not supporting it
imx-gpu-viv_6.4.0.p1.0-aarch32 does not provide virtual/libgbm and thus
a build with drm* in PACKAGECONFIG does fail.
Thus remove drm* from PACKAGECONFIG for those machines.

| ERROR: Nothing PROVIDES 'virtual/libgbm' (but .../glmark2_git.bb DEPENDS on or otherwise requires it)
| gpulib PROVIDES virtual/libgbm but was skipped: incompatible with machine

fixes: 7801868f glmark2: Remove bbappend

Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

dynamic-layers/openembedded-layer/recipes-benchmark/glmark2/glmark2_%.bbappend
@@ -1,0 +1,4 @@
+# Only _mx8 machine do provide virtual/libgbm required for any drm* flavour
+DRM-REMOVE_imxgpu = "drm-gl drm-gles2"
+DRM-REMOVE_imxgpu_mx8 = ""
+PACKAGECONFIG_remove = "${DRM-REMOVE}"