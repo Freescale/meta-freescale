Freescale/meta-freescale

commit f5d3b67e3fb463d87588f8c9452f055e5d6f5a51

xserver-xorg: enable atomic/dri3
Inspired by [1-2]

[1] https://gitlab.freedesktop.org/mesa/mesa/-/issues/4584
[2] https://gitlab.freedesktop.org/mesa/mesa/-/issues/3721

Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>
Signed-off-by: Vinicius Aquino <voa.aquino@gmail.com>

recipes-graphics/xorg-xserver/xserver-xorg_%.bbappend
@@ -11,4 +11,10 @@ IMX_OPENGL_PKGCONFIGS_REMOVE        = ""
 IMX_OPENGL_PKGCONFIGS_REMOVE_imxgpu = "glamor"
 OPENGL_PKGCONFIGS_remove_mx6        = "${IMX_OPENGL_PKGCONFIGS_REMOVE}"
 OPENGL_PKGCONFIGS_remove_mx7        = "${IMX_OPENGL_PKGCONFIGS_REMOVE}"
+OPENGL_PKGCONFIGS_remove_mx8        = "${IMX_OPENGL_PKGCONFIGS_REMOVE}"
 OPENGL_PKGCONFIGS_remove_imxdrm     = "dri glx"
+
+FILESEXTRAPATHS_prepend := "${THISDIR}/${BPN}:"
+SRC_URI_append_imxgpu2d = " file://0003-Remove-check-for-useSIGIO-option.patch"
+SRC_URI_append_use-mainline-bsp = " file://0001-Allow-to-enable-atomic-in-modesetting-DDX.patch"
+

recipes-graphics/xorg-xserver/xserver-xf86-config/use-mainline-bsp/xorg.conf
@@ -3,9 +3,11 @@ Section "Device"
     Driver     "modesetting"
     Option     "kmsdev"      "/dev/dri/card0"
     Option     "AccelMethod" "glamor"
+    Option     "Atomic"      "On"
 EndSection
 
 Section "ServerFlags"
     Option     "AutoAddGPU"  "false"
+    Option     "DRI"         "3"
 EndSection
 

recipes-graphics/xorg-xserver/xserver-xorg/0001-Allow-to-enable-atomic-in-modesetting-DDX.patch
@@ -1,0 +1,34 @@
+From 9c8b815520bf52caf780dc4defea0fe2fe8e33a3 Mon Sep 17 00:00:00 2001
+From: Daniel Abrecht <public@danielabrecht.ch>
+Date: Wed, 21 Oct 2020 21:13:30 +0200
+Subject: [PATCH] Allow to enable atomic in modesetting DDX
+
+The linux kernel blacklisted atomic for the modesetting DDX to deal with
+problems with old Xorg versions which didn't have it disabled by default.
+Ever since, it wasn't possible to enable atomic in modesetting at all
+anymore.
+
+This change doesn't enable atomic by default, but it allows a user to
+enabled it again.
+
+Signed-off-by: Daniel Abrecht <public@danielabrecht.ch>
+---
+ hw/xfree86/drivers/modesetting/driver.c | 2 +-
+ 1 file changed, 1 insertion(+), 1 deletion(-)
+
+diff --git a/hw/xfree86/drivers/modesetting/driver.c b/hw/xfree86/drivers/modesetting/driver.c
+index 6549ef8e1..bd8576f81 100644
+--- a/hw/xfree86/drivers/modesetting/driver.c
++++ b/hw/xfree86/drivers/modesetting/driver.c
+@@ -1216,7 +1216,7 @@ PreInit(ScrnInfoPtr pScrn, int flags)
+     }
+ 
+     if (xf86ReturnOptValBool(ms->drmmode.Options, OPTION_ATOMIC, FALSE)) {
+-        ret = drmSetClientCap(ms->fd, DRM_CLIENT_CAP_ATOMIC, 1);
++        ret = drmSetClientCap(ms->fd, DRM_CLIENT_CAP_ATOMIC, 2);
+         ms->atomic_modeset = (ret == 0);
+     } else {
+         ms->atomic_modeset = FALSE;
+-- 
+GitLab
+

commit f4001e22cc3278acf65919ffdcb120fae863c141

xserver-xorg: Enable glamor for i.MX 8
glamor acceleration for i.MX 8 with GPU was dropped accidentally.

Fixes: f5d3b67
Signed-off-by: Tom Hochstein <tom.hochstein@nxp.com>

recipes-graphics/xorg-xserver/xserver-xorg_%.bbappend
@@ -13,5 +13,4 @@ IMX_OPENGL_PKGCONFIGS_REMOVE        = ""
 IMX_OPENGL_PKGCONFIGS_REMOVE_imxgpu = "glamor"
 OPENGL_PKGCONFIGS_remove_mx6        = "${IMX_OPENGL_PKGCONFIGS_REMOVE}"
 OPENGL_PKGCONFIGS_remove_mx7        = "${IMX_OPENGL_PKGCONFIGS_REMOVE}"
-OPENGL_PKGCONFIGS_remove_mx8        = "${IMX_OPENGL_PKGCONFIGS_REMOVE}"
 OPENGL_PKGCONFIGS_remove_imxdrm     = "dri glx"