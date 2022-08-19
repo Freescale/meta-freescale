Freescale/meta-freescale

commit e7efa7a2b4db4f6df156527b2ef35c70ddda9d06

imx-kobs: Update SRCREV to 02c01d
Signed-off-by: Alexandru Palalau <ioan-alexandru.palalau@nxp.com>

recipes-bsp/imx-kobs/imx-kobs_git.bb
@@ -1,5 +1,5 @@
 # Copyright (C) 2013-2016 Freescale Semiconductor
-# Copyright 2017-2018 NXP
+# Copyright (C) 2017-2019 NXP
 # Copyright 2018 (C) O.S. Systems Software LTDA.
 
 SUMMARY = "Nand boot write source"
@@ -9,9 +9,8 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=393a5ca445f6965873eca0259a17f833"
 
 PV = "5.5+git${SRCPV}"
 SRC_URI = "git://github.com/NXPmicro/imx-kobs.git;protocol=https \
-           file://0001-Add-missing-includes-as-pointed-out-by-musl.patch \
 "
-SRCREV = "a0e9adce2fb7fcd57e794d7f9a5deba0f94f521b"
+SRCREV = "02c01d4a3d85d767360820a23751cd1aa231d0a0"
 S = "${WORKDIR}/git"
 
 inherit autotools pkgconfig

commit 2902c1c5a26b9ac2c7a1957698d92ceea2c5b0df

imx-kobs: apply existing patch to fix musl build
Long time ago, a bump ended removing the patch which fixes the musl
build and now it is being added back.

Fixes: e7efa7a2 "imx-kobs: Update SRCREV to 02c01d"
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

recipes-bsp/imx-kobs/imx-kobs_git.bb
@@ -9,6 +9,7 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=393a5ca445f6965873eca0259a17f833"
 
 PV = "5.5+git${SRCPV}"
 SRC_URI = "git://github.com/NXPmicro/imx-kobs.git;protocol=https;branch=master \
+           file://0001-Add-missing-includes-as-pointed-out-by-musl.patch \
 "
 SRCREV = "269fdffcf97238684de9f28977a73677282e061f"
 S = "${WORKDIR}/git"