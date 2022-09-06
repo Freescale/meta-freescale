Freescale/meta-freescale

commit 49dac9d70e19a3d2b83770067352399f2cd05761

mesa-demos: Update for latest
Drop OpenVG packageconfig and patch as OpenVG support is dropped from
mesa-demos.

For i.MX GPU:
- x11 is not required
- GLX is not supported
- GLU is required
- GLUT is not required for x11

Since x11 is not required but GLU is, restore the PACKAGECONFIG for glu,
which was recently tied to x11.

Signed-off-by: Tom Hochstein <tom.hochstein@nxp.com>
(cherry picked from commit ed60a7915dfb4c2da89007c1670ef1da8618a063)

recipes-graphics/mesa/mesa-demos/Add-OpenVG-demos-to-support-wayland.patch
@@ -1,84 +1,0 @@
-From 8aba54422d9a77383c150f9f70240b18b6e1918e Mon Sep 17 00:00:00 2001
-From: Prabhu Sundararaj <prabhu.sundararaj@freescale.com>
-Date: Thu, 9 Apr 2015 15:47:21 -0500
-Subject: [PATCH] Add OpenVG demos to support wayland.
-
-Upstream-Status: Pending
-
-Signed-off-by: Prabhu Sundararaj <prabhu.sundararaj@freescale.com>
----
- src/egl/Makefile.am        |  6 +++---
- src/egl/openvg/Makefile.am | 33 +++++++++++++++++++++++++++++----
- 2 files changed, 32 insertions(+), 7 deletions(-)
-
-Index: mesa-demos-8.2.0/src/egl/Makefile.am
-===================================================================
---- mesa-demos-8.2.0.orig/src/egl/Makefile.am	2016-05-09 11:45:51.479100180 -0500
-+++ mesa-demos-8.2.0/src/egl/Makefile.am	2016-05-09 11:45:51.475100160 -0500
-@@ -26,10 +26,10 @@
- 	eglut \
- 	opengles1 \
- 	opengles2 \
--	oes_vg
-+	oes_vg \
-+        openvg
- 
- if HAVE_GLU
- SUBDIRS += \
--	opengl \
--	openvg
-+	opengl
- endif
-Index: mesa-demos-8.2.0/src/egl/openvg/Makefile.am
-===================================================================
---- mesa-demos-8.2.0.orig/src/egl/openvg/Makefile.am	2016-05-09 11:45:51.479100180 -0500
-+++ mesa-demos-8.2.0/src/egl/openvg/Makefile.am	2016-05-09 12:39:30.000000000 -0500
-@@ -47,13 +47,26 @@
- endif
- endif
- 
-+if HAVE_WAYLAND
-+EGL_WL_DEMOS = \
-+    lion_wayland \
-+    sp_wayland
-+
-+if HAVE_FREETYPE2
-+EGL_WL_DEMOS += \
-+	vgtext_wayland
-+endif
-+endif
-+
- if HAVE_EGL
- if HAVE_VG
- bin_PROGRAMS = \
--	$(EGL_X11_DEMOS)
-+	$(EGL_X11_DEMOS) \
-+	$(EGL_WL_DEMOS)
- endif
- endif
- 
-+if HAVE_X11
- lion_x11_SOURCES = lion.c lion-render.c lion-render.h
- sp_x11_SOURCES = sp.c
- 
-@@ -63,6 +76,20 @@
- text_SOURCES = text.c
- text_CFLAGS = $(AM_CFLAGS) @FREETYPE2_CFLAGS@
- text_LDADD = @FREETYPE2_LIBS@ ../eglut/libeglut_x11.la
-+endif
-+
-+if HAVE_WAYLAND
-+lion_wayland_SOURCES = lion.c lion-render.c lion-render.h
-+lion_wayland_LDADD = ../eglut/libeglut_wayland.la
-+
-+sp_wayland_SOURCES = sp.c
-+sp_wayland_LDADD = ../eglut/libeglut_wayland.la
-+
-+vgtext_wayland_SOURCES = text.c
-+vgtext_wayland_CFLAGS = $(AM_CFLAGS) @FREETYPE2_CFLAGS@
-+vgtext_wayland_LDADD = @FREETYPE2_LIBS@ ../eglut/libeglut_wayland.la
-+
-+endif
- 
- SUBDIRS = \
- 	trivial

recipes-graphics/mesa/mesa-demos_%.bbappend
@@ -2,8 +2,9 @@ FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
 
 SRC_URI:append:imxgpu = " \
     file://Replace-glWindowPos2iARB-calls-with-glWindowPos2i.patch \
-    file://fix-clear-build-break.patch \
-    file://Add-OpenVG-demos-to-support-wayland.patch"
+    file://fix-clear-build-break.patch"
+
+REQUIRED_DISTRO_FEATURES:remove:imxgpu = "x11"
 
 PACKAGECONFIG:remove = " \
     ${PACKAGECONFIG_REMOVE_IF_2D_ONLY} \
@@ -12,13 +13,11 @@ PACKAGECONFIG_REMOVE_IF_2D_ONLY          = ""
 PACKAGECONFIG_REMOVE_IF_2D_ONLY:imxgpu2d = "gles1 gles2"
 PACKAGECONFIG_REMOVE_IF_2D_ONLY:imxgpu3d = ""
 PACKAGECONFIG_REMOVE_IF_GPU              = ""
-PACKAGECONFIG_REMOVE_IF_GPU:imxgpu       = " \
-    ${@bb.utils.contains('DISTRO_FEATURES', 'wayland', 'glu x11', '', d)}"
+PACKAGECONFIG_REMOVE_IF_GPU:imxgpu       = "glx x11"
 
 PACKAGECONFIG:append = " \
     ${PACKAGECONFIG_APPEND_IF_GPU}"
 PACKAGECONFIG_APPEND_IF_GPU        = ""
-PACKAGECONFIG_APPEND_IF_GPU:imxgpu = " \
-    ${@bb.utils.contains('DISTRO_FEATURES', 'wayland', 'wayland vg', '', d)} \
-    ${@bb.utils.contains('DISTRO_FEATURES',     'x11',       'glut', '', d)}"
-"
+PACKAGECONFIG_APPEND_IF_GPU:imxgpu = "glu"
+
+PACKAGECONFIG[glu] = ",,libglu"

commit cce409d7bff5ecd8851027b00a048152c85cee18

mesa-demos: Revert backported 8.5 changes
Some of the changes backported recently to kirkstone are specific to
8.5, but kirkstone uses 8.4. Specifically, on 8.4:
- OpenVG is still supported
- wayland packageconfig is not added by default
- glu packageconfig still exists separate from x11 packageconfig

Fixes: 49dac9d7
Signed-off-by: Tom Hochstein <tom.hochstein@nxp.com>

recipes-graphics/mesa/mesa-demos/Add-OpenVG-demos-to-support-wayland.patch
@@ -1,0 +1,84 @@
+From 8aba54422d9a77383c150f9f70240b18b6e1918e Mon Sep 17 00:00:00 2001
+From: Prabhu Sundararaj <prabhu.sundararaj@freescale.com>
+Date: Thu, 9 Apr 2015 15:47:21 -0500
+Subject: [PATCH] Add OpenVG demos to support wayland.
+
+Upstream-Status: Pending
+
+Signed-off-by: Prabhu Sundararaj <prabhu.sundararaj@freescale.com>
+---
+ src/egl/Makefile.am        |  6 +++---
+ src/egl/openvg/Makefile.am | 33 +++++++++++++++++++++++++++++----
+ 2 files changed, 32 insertions(+), 7 deletions(-)
+
+Index: mesa-demos-8.2.0/src/egl/Makefile.am
+===================================================================
+--- mesa-demos-8.2.0.orig/src/egl/Makefile.am	2016-05-09 11:45:51.479100180 -0500
++++ mesa-demos-8.2.0/src/egl/Makefile.am	2016-05-09 11:45:51.475100160 -0500
+@@ -26,10 +26,10 @@
+ 	eglut \
+ 	opengles1 \
+ 	opengles2 \
+-	oes_vg
++	oes_vg \
++        openvg
+ 
+ if HAVE_GLU
+ SUBDIRS += \
+-	opengl \
+-	openvg
++	opengl
+ endif
+Index: mesa-demos-8.2.0/src/egl/openvg/Makefile.am
+===================================================================
+--- mesa-demos-8.2.0.orig/src/egl/openvg/Makefile.am	2016-05-09 11:45:51.479100180 -0500
++++ mesa-demos-8.2.0/src/egl/openvg/Makefile.am	2016-05-09 12:39:30.000000000 -0500
+@@ -47,13 +47,26 @@
+ endif
+ endif
+ 
++if HAVE_WAYLAND
++EGL_WL_DEMOS = \
++    lion_wayland \
++    sp_wayland
++
++if HAVE_FREETYPE2
++EGL_WL_DEMOS += \
++	vgtext_wayland
++endif
++endif
++
+ if HAVE_EGL
+ if HAVE_VG
+ bin_PROGRAMS = \
+-	$(EGL_X11_DEMOS)
++	$(EGL_X11_DEMOS) \
++	$(EGL_WL_DEMOS)
+ endif
+ endif
+ 
++if HAVE_X11
+ lion_x11_SOURCES = lion.c lion-render.c lion-render.h
+ sp_x11_SOURCES = sp.c
+ 
+@@ -63,6 +76,20 @@
+ text_SOURCES = text.c
+ text_CFLAGS = $(AM_CFLAGS) @FREETYPE2_CFLAGS@
+ text_LDADD = @FREETYPE2_LIBS@ ../eglut/libeglut_x11.la
++endif
++
++if HAVE_WAYLAND
++lion_wayland_SOURCES = lion.c lion-render.c lion-render.h
++lion_wayland_LDADD = ../eglut/libeglut_wayland.la
++
++sp_wayland_SOURCES = sp.c
++sp_wayland_LDADD = ../eglut/libeglut_wayland.la
++
++vgtext_wayland_SOURCES = text.c
++vgtext_wayland_CFLAGS = $(AM_CFLAGS) @FREETYPE2_CFLAGS@
++vgtext_wayland_LDADD = @FREETYPE2_LIBS@ ../eglut/libeglut_wayland.la
++
++endif
+ 
+ SUBDIRS = \
+ 	trivial

recipes-graphics/mesa/mesa-demos_%.bbappend
@@ -2,7 +2,8 @@ FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
 
 SRC_URI:append:imxgpu = " \
     file://Replace-glWindowPos2iARB-calls-with-glWindowPos2i.patch \
-    file://fix-clear-build-break.patch"
+    file://fix-clear-build-break.patch \
+    file://Add-OpenVG-demos-to-support-wayland.patch"
 
 REQUIRED_DISTRO_FEATURES:remove:imxgpu = "x11"
 
@@ -18,5 +19,7 @@ PACKAGECONFIG_REMOVE_IF_GPU:imxgpu       = "glx x11"
 PACKAGECONFIG:append = " \
     ${PACKAGECONFIG_APPEND_IF_GPU}"
 PACKAGECONFIG_APPEND_IF_GPU        = ""
+PACKAGECONFIG_APPEND_IF_GPU:imxgpu = " \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'wayland', 'wayland vg', '', d)}"
 
-PACKAGECONFIG[glu] = ",,libglu"
+PACKAGECONFIG[glu] = "--enable-glu,--disable-glu,virtual/libgl libglu"