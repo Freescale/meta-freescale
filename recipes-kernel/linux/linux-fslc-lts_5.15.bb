Freescale/meta-freescale

commit 9f2c6a408f7fa6037ff155321bba4d91928e3367

linux-fslc-lts: update to v5.15.91
Kernel repository has been upgraded up to v5.15.91 from stable korg.

Relevant changes:
- 4d20f1dcda24 Merge pull request #622 from MrCry0/5.15.x+fslc
- dd6ac8bf3555 Merge tag 'v5.15.91' into 5.15.x+fslc
- 9cf4111cdf94 Linux 5.15.91
- 14cc13e433e1 perf/x86/amd: fix potential integer overflow on shift of a int
- 033636b32258 netfilter: conntrack: unify established states for SCTP paths
- 0b08201158f1 x86/i8259: Mark legacy PIC interrupts with IRQ_LEVEL
- b57740036792 block: fix and cleanup bio_check_ro
- 1d152437e46f kbuild: Allow kernel installation packaging to override pkg-config
- a1964688582d cpufreq: governor: Use kobject release() method to free dbs_data
- 7c513ced0dec cpufreq: Move to_gov_attr_set() to cpufreq.h
- cf7a08622d2b Revert "Input: synaptics - switch touchpad on HP Laptop 15-da3001TU to RMI mode"
- 53c5d61198c1 tools: gpio: fix -c option of gpio-event-mon
- a7d1a303ff0f treewide: fix up files incorrectly marked executable
- 046fe53907c5 net: mdio-mux-meson-g12a: force internal PHY off on mux switch
- 86bdccde7842 net/tg3: resolve deadlock in tg3_reset_task() during EEH
- 4364bf79d829 thermal: intel: int340x: Add locking to int340x_thermal_get_trip_type()
- e69c3a0d9d3d net: mctp: mark socks as dead on unhash, prevent re-add
- 954cc215cd7a net: ravb: Fix possible hang if RIS2_QFF1 happen
- 0f7218bf0a00 net: ravb: Fix lack of register setting after system resumed for Gen3
- 3db4ca2938eb ravb: Rename "no_ptp_cfg_active" and "ptp_cfg_active" variables
- 621f296f11cf gpio: mxc: Unlock on error path in mxc_flip_edge()
- 071a8392869f nvme: fix passthrough csi check
- 614471b7f7cd riscv/kprobe: Fix instruction simulation of JALR
- 3391bd42351b sctp: fail if no bound addresses can be used for a given scope
- b0784860e145 net/sched: sch_taprio: do not schedule in taprio_reset()
- d2d3ab1b1de3 netrom: Fix use-after-free of a listening socket.
- 9df5ab02c65e netfilter: conntrack: fix vtag checks for ABORT/SHUTDOWN_COMPLETE

recipes-kernel/linux/linux-fslc-lts_5.15.bb
@@ -22,6 +22,6 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=6bc538ed5bd9a7fc9398086aedcd7e46"
 LINUX_VERSION = "5.15.86"
 
 KBRANCH = "5.15.x+fslc"
-SRCREV = "a86fe5cb4fe96228e358108d0ed91d2c49bce097"
+SRCREV = "4d20f1dcda24d6b2ee234e91bf2ba00083058586"
 
 COMPATIBLE_MACHINE = "(imx-generic-bsp)"

commit 844e203360e00bf9c26f7f1cc61ebb7ed8f50feb

linux-fslc-lts: fix linux version
Missed bumping LINUX_VERSION. Fix it.

Fixes: 9f2c6a40 ("linux-fslc-lts: update to v5.15.91")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

recipes-kernel/linux/linux-fslc-lts_5.15.bb
@@ -19,7 +19,7 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=6bc538ed5bd9a7fc9398086aedcd7e46"
 #
 # LINUX_VERSION define should match to the kernel version referenced by SRC_URI and
 # should be updated once patchlevel is merged.
-LINUX_VERSION = "5.15.86"
+LINUX_VERSION = "5.15.91"
 
 KBRANCH = "5.15.x+fslc"
 SRCREV = "4d20f1dcda24d6b2ee234e91bf2ba00083058586"