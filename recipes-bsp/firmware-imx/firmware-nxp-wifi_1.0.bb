Freescale/meta-freescale

commit f9ce2b8510e5cf6f6a3b87aac5ad59383d4b6fcc

firmware-imx: firmware-nxp-wifi: remove unneeded commands
Do not use separate commands to install target directories. Instead,
add a proper option when files are installed.

Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

recipes-bsp/firmware-imx/firmware-nxp-wifi_1.0.bb
@@ -26,17 +26,13 @@ do_compile() {
 }
 
 do_install() {
-    install -d ${D}${sysconfdir}/firmware
-
     # Install various flavors of Broadcom firmware provided by Murata:
     # - bcm4359-pcie
-    install -d ${D}${nonarch_base_libdir}/firmware/brcm
-    install -m 0644 cyw-wifi-bt/*_CYW*/brcmfmac4359-pcie* ${D}${nonarch_base_libdir}/firmware/brcm
+    install -D -m 0644 cyw-wifi-bt/*_CYW*/brcmfmac4359-pcie* ${D}${nonarch_base_libdir}/firmware/brcm
     install -m 0644 cyw-wifi-bt/*_CYW*/BCM4349B1*.hcd ${D}${sysconfdir}/firmware
 
     # Install NXP Connectivity common
-    install -d ${D}${nonarch_base_libdir}/firmware/nxp
-    install -m 0644 nxp/wifi_mod_para.conf ${D}${nonarch_base_libdir}/firmware/nxp
+    install -D -m 0644 nxp/wifi_mod_para.conf ${D}${nonarch_base_libdir}/firmware/nxp
 
     # Install NXP Connectivity SD8801 firmware
     install -m 0644 nxp/FwImage_8801_SD/ed_mac_ctrl_V1_8801.conf  ${D}${nonarch_base_libdir}/firmware/nxp

commit 4c2a6df26ca8e90191fc6aad22cacbb25ea4c6ae

firmware-nxp-wifi: Fix install -D
The install -D option does unfortunately have idiosyncratic
requirements on the destination format. If you don't get the format
right, it fails:
```
install: target 'tmp/work/all-poky-linux/firmware-nxp-wifi/1.0-r0/image/lib/firmware/brcm' is not a directory
```

This commit also fixes a missing -D option.

Fixes: f9ce2b8
Signed-off-by: Tom Hochstein <tom.hochstein@nxp.com>

recipes-bsp/firmware-imx/firmware-nxp-wifi_1.0.bb
@@ -28,11 +28,16 @@ do_compile() {
 do_install() {
     # Install various flavors of Broadcom firmware provided by Murata:
     # - bcm4359-pcie
-    install -D -m 0644 cyw-wifi-bt/*_CYW*/brcmfmac4359-pcie* ${D}${nonarch_base_libdir}/firmware/brcm
-    install -m 0644 cyw-wifi-bt/*_CYW*/BCM4349B1*.hcd ${D}${sysconfdir}/firmware
+    for f in cyw-wifi-bt/*_CYW*/brcmfmac4359-pcie*; do
+        install -D -m 0644 $f ${D}${nonarch_base_libdir}/firmware/brcm/$(basename $f)
+    done
+
+    for f in cyw-wifi-bt/*_CYW*/BCM4349B1*.hcd; do
+        install -D -m 0644 $f ${D}${sysconfdir}/firmware/$(basename $f)
+    done
 
     # Install NXP Connectivity common
-    install -D -m 0644 nxp/wifi_mod_para.conf ${D}${nonarch_base_libdir}/firmware/nxp
+    install -D -m 0644 nxp/wifi_mod_para.conf ${D}${nonarch_base_libdir}/firmware/nxp/wifi_mod_para.conf
 
     # Install NXP Connectivity SD8801 firmware
     install -m 0644 nxp/FwImage_8801_SD/ed_mac_ctrl_V1_8801.conf  ${D}${nonarch_base_libdir}/firmware/nxp