Freescale/meta-freescale

commit e513c530c4884855094c6e6cbbef168b310b1005

firmware-nxp-wifi: Update lf-5.15.71-2.2.0 to lf-6.1.1_1.0.0
Add installing firmware for IW416 and IW612.

Relevant changes:
- bacbeb4 MA-2102-1 [Android-powersave-image] Remove android_wifi_mod_para_powersave.conf
- 09b5e3d FwImage: update firmware to mxm5x17368.p2
- 6b9248a FwImage: IW612: rename the IW612 firmware subdirectory
- 87d2442 FwImage: update firmware to mxm5x17368
- 6fa6713 EULA.txt: update Software License to v40
- 5c66cfa FwImage: SD8801: change the ed_mac value for muRata 2DS module

Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

recipes-bsp/firmware-imx/firmware-nxp-wifi_1.0.bb
@@ -11,8 +11,8 @@ LICENSE = "Proprietary"
 LIC_FILES_CHKSUM = "file://EULA.txt;md5=be5ff43682ed6c57dfcbeb97651c2829"
 
 SRC_URI = "git://github.com/NXP/imx-firmware.git;protocol=https;branch=${SRCBRANCH}"
-SRCBRANCH = "lf-5.15.71_2.2.0"
-SRCREV = "982bb10dfabfb9e7b9dc106c59a4fbb2c45bfb44"
+SRCBRANCH = "lf-6.1.1_1.0.0"
+SRCREV = "bacbeb4789c1b13d13aab12ada29217ce8c3e905"
 
 S = "${WORKDIR}/git"
 
@@ -60,6 +60,12 @@ do_install() {
 
     # Install NXP Connectivity SDIO9098 firmware
     install -m 0644 nxp/FwImage_9098_SD/sdiouart9098_combo_v1.bin ${D}${nonarch_base_libdir}/firmware/nxp
+
+    # Install NXP Connectivity IW416 firmware
+    install -m 0644 nxp/FwImage_IW416_SD/sdiouartiw416_combo_v0.bin ${D}${nonarch_base_libdir}/firmware/nxp
+
+    # Install NXP Connectivity IW612 firmware
+    install -m 0644 nxp/FwImage_IW612_SD/sduart_nw61x_v1.bin.se  ${D}${nonarch_base_libdir}/firmware/nxp
 }
 
 PACKAGES =+ " \

commit d230a8b1f603d01c6eb1220d0066f3a90a75f65a

firmware-nxp-wifi: Fix iw416 and iw612 additions
Complete the installation for the just-added iw416 and iw612 and add
the packaging.

Fixes: e513c53
Signed-off-by: Tom Hochstein <tom.hochstein@nxp.com>

recipes-bsp/firmware-imx/firmware-nxp-wifi_1.0.bb
@@ -67,10 +67,17 @@ do_install() {
     install -m 0644 nxp/FwImage_9098_SD/sdiouart9098_combo_v1.bin ${D}${nonarch_base_libdir}/firmware/nxp
 
     # Install NXP Connectivity IW416 firmware
+    install -m 0644 nxp/FwImage_IW416_SD/sdioiw416_wlan_v0.bin      ${D}${nonarch_base_libdir}/firmware/nxp
     install -m 0644 nxp/FwImage_IW416_SD/sdiouartiw416_combo_v0.bin ${D}${nonarch_base_libdir}/firmware/nxp
+    install -m 0644 nxp/FwImage_IW416_SD/uartiw416_bt_v0.bin        ${D}${nonarch_base_libdir}/firmware/nxp
 
     # Install NXP Connectivity IW612 firmware
-    install -m 0644 nxp/FwImage_IW612_SD/sduart_nw61x_v1.bin.se  ${D}${nonarch_base_libdir}/firmware/nxp
+    install -m 0644 nxp/FwImage_IW612_SD/sduart_nw61x_v1.bin.se ${D}${nonarch_base_libdir}/firmware/nxp
+    install -m 0644 nxp/FwImage_IW612_SD/sd_w61x_v1.bin.se      ${D}${nonarch_base_libdir}/firmware/nxp
+    install -m 0644 nxp/FwImage_IW612_SD/uartspi_n61x_v1.bin.se ${D}${nonarch_base_libdir}/firmware/nxp
+    for f in nxp/FwImage_IW612_SD/IW612_SD_RFTest/*; do
+        install -D -m 0644 $f ${D}${nonarch_base_libdir}/firmware/nxp/IW612_SD_RFTest/$(basename $f)
+    done
 }
 
 PACKAGES =+ " \
@@ -83,6 +90,8 @@ PACKAGES =+ " \
     ${PN}-nxp8997-sdio \
     ${PN}-nxp9098-pcie \
     ${PN}-nxp9098-sdio \
+    ${PN}-nxpiw416-sdio \
+    ${PN}-nxpiw612-sdio \
 "
 
 FILES:${PN}-bcm4359-pcie = " \
@@ -131,3 +140,16 @@ FILES:${PN}-nxp9098-sdio = " \
     ${nonarch_base_libdir}/firmware/nxp/sdiouart9098* \
 "
 RDEPENDS:${PN}-nxp9098-sdio += "${PN}-nxp-common"
+
+FILES:${PN}-nxpiw416-sdio = " \
+    ${nonarch_base_libdir}/firmware/nxp/*iw416* \
+"
+RDEPENDS:${PN}-nxpiw416-sdio += "${PN}-nxp-common"
+
+FILES:${PN}-nxpiw612-sdio = " \
+    ${nonarch_base_libdir}/firmware/nxp/sduart_nw61x_v1.bin.se \
+    ${nonarch_base_libdir}/firmware/nxp/sd_w61x_v1.bin.se \
+    ${nonarch_base_libdir}/firmware/nxp/uartspi_n61x_v1.bin.se \
+    ${nonarch_base_libdir}/firmware/nxp/IW612_SD_RFTest/ \
+"
+RDEPENDS:${PN}-nxpiw612-sdio += "${PN}-nxp-common"