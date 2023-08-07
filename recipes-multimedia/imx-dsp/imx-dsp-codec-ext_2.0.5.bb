Freescale/meta-freescale

commit 707e624d3c048bcfc1d3225b4ae91238c96209d5

imx-dsp-codec-ext: Upgrade 2.0.2 -> 2.0.5
- Add more DSP libraries of i.MX platforms.
- Install different lib of different platforms.

Signed-off-by: Zelan Zou <zelan.zou@nxp.com>
Signed-off-by: Jun Zhu <junzhu@nxp.com>

recipes-multimedia/imx-dsp/imx-dsp-codec-ext_2.0.5.bb
@@ -1,15 +1,21 @@
-# Copyright 2018-2021 NXP
+# Copyright 2018-2023 NXP
 
 DESCRIPTION = "i.MX DSP Codec Wrapper and Lib owned by NXP"
 LICENSE = "Proprietary"
-LIC_FILES_CHKSUM = "file://COPYING;md5=5a0bf11f745e68024f37b4724a5364fe"
+LIC_FILES_CHKSUM = "file://COPYING;md5=63a38e9f392d8813d6f1f4d0d6fbe657"
 
 inherit fsl-eula-unpack autotools pkgconfig
 
 SRC_URI = "${FSL_MIRROR}/${BP}.bin;fsl-eula=true"
 
-SRC_URI[md5sum] = "80f1ea02e8af55f08efcc1014b51565b"
-SRC_URI[sha256sum] = "3575152bb1372842b07794e7daafb8282827ce301c1c944bd1c2b5fc12120671"
+SRC_URI[md5sum] = "2f79f786604ef4d12d1682610353555a"
+SRC_URI[sha256sum] = "b138fcda29be9ed7939d05365af3ac94b0fa6e35e56593168a9d4eed58498a8f"
+
+EXTRA_OECONF:append:mx8qm-nxp-bsp = " --enable-imx8qmqxp"
+EXTRA_OECONF:append:mx8qxp-nxp-bsp = " --enable-imx8qmqxp"
+EXTRA_OECONF:append:mx8dx-nxp-bsp = " --enable-imx8qmqxp"
+EXTRA_OECONF:append:mx8mp-nxp-bsp = " --enable-imx8m"
+EXTRA_OECONF:append:mx8ulp-nxp-bsp = " --enable-imx8ulp"
 
 # Fix strip command failed: 'Unable to recognise the format of the input file'
 INHIBIT_PACKAGE_STRIP = "1"

commit 7416b762925207d08e3964f9c4ce320a8157b501

imx-dsp-codec-ext: fix missing gnu_hash
Skip qa test on GNU_HASH. At least some prebuilt binaries lack it.

Fixes: 707e624d ("imx-dsp-codec-ext: Upgrade 2.0.2 -> 2.0.5")

| ERROR: imx-dsp-codec-ext-2.0.5-r0 do_package_qa: QA Issue: File /usr/lib/imx-mm/audio-codec/dsp/lib_nbamrd_wrap_dsp.so.1 in package imx-dsp-codec-ext doesn't have GNU_HASH (didn't pass LDFLAGS?)
| File /usr/lib/imx-mm/audio-codec/dsp/lib_mp3d_wrap_dsp.so.3 in package imx-dsp-codec-ext doesn't have GNU_HASH (didn't pass LDFLAGS?)
| File /usr/lib/imx-mm/audio-codec/dsp/lib_aacd_wrap_dsp.so.3 in package imx-dsp-codec-ext doesn't have GNU_HASH (didn't pass LDFLAGS?)
| File /usr/lib/imx-mm/audio-codec/dsp/lib_vorbisd_wrap_dsp.so.3 in package imx-dsp-codec-ext doesn't have GNU_HASH (didn't pass LDFLAGS?)
| File /usr/lib/imx-mm/audio-codec/dsp/lib_wbamrd_wrap_dsp.so.1 in package imx-dsp-codec-ext doesn't have GNU_HASH (didn't pass LDFLAGS?) [ldflags]
| ERROR: imx-dsp-codec-ext-2.0.5-r0 do_package_qa: Fatal QA errors were found, failing task.
| ERROR: Logfile of failure stored in: /build/krm/oe-core_master/build/tmp/work/armv8a-tdx-linux/imx-dsp-codec-ext/2.0.5-r0/temp/log.do_package_qa.1942699
| ERROR: Task (/build/krm/oe-core_master/build/../layers/meta-freescale/recipes-multimedia/imx-dsp/imx-dsp-codec-ext_2.0.5.bb:do_package_qa) failed with exit code '1'

Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-multimedia/imx-dsp/imx-dsp-codec-ext_2.0.5.bb
@@ -21,7 +21,7 @@ EXTRA_OECONF:append:mx8ulp-nxp-bsp = " --enable-imx8ulp"
 INHIBIT_PACKAGE_STRIP = "1"
 INHIBIT_SYSROOT_STRIP = "1"
 INHIBIT_PACKAGE_DEBUG_SPLIT = "1"
-INSANE_SKIP:${PN} = "arch dev-so"
+INSANE_SKIP:${PN} = "arch dev-so ldflags"
 
 FILES:${PN} += "${libdir}/imx-mm/audio-codec ${datadir}/imx-mm"
 COMPATIBLE_MACHINE = "(mx8qm-nxp-bsp|mx8qxp-nxp-bsp|mx8dx-nxp-bsp|mx8mp-nxp-bsp|mx8ulp-nxp-bsp)"