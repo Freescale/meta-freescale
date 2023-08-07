Freescale/meta-freescale

commit 73d43d8126d8b77c871413ef2263af919ff66c66

imx-vpuwrap: Upgrade to 6.1.22-2.0.0
Signed-off-by: Zelan Zou <zelan.zou@nxp.com>
Signed-off-by: Jun Zhu <junzhu@nxp.com>

recipes-multimedia/imx-vpuwrap/imx-vpuwrap_git.bb
@@ -1,5 +1,5 @@
 # Copyright (C) 2013-2016 Freescale Semiconductor
-# Copyright 2017-2022 NXP
+# Copyright 2017-2023 NXP
 # Released under the MIT license (see COPYING.MIT for the terms)
 
 DESCRIPTION = "Freescale Multimedia VPU wrapper"
@@ -11,8 +11,8 @@ DEPENDS = "virtual/imxvpu"
 DEPENDS:append:mx8mp-nxp-bsp = " imx-vpu-hantro-vc"
 
 SRC_URI = "git://github.com/NXP/imx-vpuwrap.git;protocol=https;branch=${SRCBRANCH}"
-SRCBRANCH = "MM_04.07.03_2301_L6.1.y"
-SRCREV = "12a78274b3bc4f3105aabe16df03edd5e0c3b84a"
+SRCBRANCH = "MM_04.08.00_2305_L6.1.y"
+SRCREV = "a9cd0835da0ecce55d94510d028371b473090fcc"
 
 S = "${WORKDIR}/git"
 

commit 1f4f98ed5727ebe140962668a8462b36b6eedbeb

imx-vpuwrap: fix license checksum
The license was updated to LA_OPT_NXP_Software_License v45 May 2023.
Follow that.

Fixes: 73d43d81 ("imx-vpuwrap: Upgrade to 6.1.22-2.0.0")
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-multimedia/imx-vpuwrap/imx-vpuwrap_git.bb
@@ -5,7 +5,7 @@
 DESCRIPTION = "Freescale Multimedia VPU wrapper"
 LICENSE = "Proprietary"
 SECTION = "multimedia"
-LIC_FILES_CHKSUM = "file://COPYING;md5=ea25d099982d035af85d193c88a1b479"
+LIC_FILES_CHKSUM = "file://COPYING;md5=63a38e9f392d8813d6f1f4d0d6fbe657"
 
 DEPENDS = "virtual/imxvpu"
 DEPENDS:append:mx8mp-nxp-bsp = " imx-vpu-hantro-vc"