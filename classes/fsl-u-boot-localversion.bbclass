Freescale/meta-freescale

commit d7e13f19fbf93cb0bd87c29b44c5012e2e0e4393

fsl-u-boot-localversion.bbclass: fix SRCREV_machine and AUTOREV use cases
Use the same logic as fsl-kernel-localversion.bbclass:
    commit 16a356ef0d5d62090e84530e125ea6952994aaeb

Signed-off-by: Jun Zhu <junzhu@nxp.com>

classes/fsl-u-boot-localversion.bbclass
@@ -17,11 +17,21 @@ UBOOT_LOCALVERSION = "${LOCALVERSION}"
 do_compile:prepend() {
 	if [ "${SCMVERSION}" = "y" ]; then
 		# Add GIT revision to the local version
-		head=`cd ${S} ; git rev-parse --verify --short ${SRCREV} 2> /dev/null`
-		patches=`cd ${S} ; git rev-list --count ${SRCREV}..HEAD 2> /dev/null`
-		printf "%s%s%s%s%s" "${UBOOT_LOCALVERSION}" +g $head +p $patches > ${S}/.scmversion
-		printf "%s%s%s%s%s" "${UBOOT_LOCALVERSION}" +g $head +p $patches > ${B}/.scmversion
-    else
+                if [ "${SRCREV}" = "INVALID" ]; then
+                        hash=${SRCREV_machine}
+                else
+                        hash=${SRCREV}
+                fi
+                if [ "$hash" = "AUTOINC" ]; then
+                        branch=`git --git-dir=${S}/.git  symbolic-ref --short -q HEAD`
+                        head=`git --git-dir=${S}/.git rev-parse --verify --short origin/${branch} 2> /dev/null`
+                else
+                        head=`git --git-dir=${S}/.git rev-parse --verify --short $hash 2> /dev/null`
+                fi
+                patches=`git --git-dir=${S}/.git rev-list --count $head..HEAD 2> /dev/null`
+                printf "%s%s%s%s" +g $head +p $patches > ${S}/.scmversion
+                printf "%s%s%s%s" +g $head +p $patches > ${B}/.scmversion
+        else
 		printf "%s" "${UBOOT_LOCALVERSION}" > ${S}/.scmversion
 		printf "%s" "${UBOOT_LOCALVERSION}" > ${B}/.scmversion
 	fi

commit 8c5fbbce4d6260146d6e4c8b8f6f4a032c8f0a4f

fsl-u-boot-localversion.bbclass: Unify indentations
Fixes: d7e13f19fbf9 ("fsl-u-boot-localversion.bbclass: fix SRCREV_machine and AUTOREV use cases")
Signed-off-by: Piotr Wojtaszczyk <piotr.wojtaszczyk@timesys.com>

classes/fsl-u-boot-localversion.bbclass
@@ -17,21 +17,21 @@ UBOOT_LOCALVERSION = "${LOCALVERSION}"
 do_compile:prepend() {
 	if [ "${SCMVERSION}" = "y" ]; then
 		# Add GIT revision to the local version
-                if [ "${SRCREV}" = "INVALID" ]; then
-                        hash=${SRCREV_machine}
-                else
-                        hash=${SRCREV}
-                fi
-                if [ "$hash" = "AUTOINC" ]; then
-                        branch=`git --git-dir=${S}/.git  symbolic-ref --short -q HEAD`
-                        head=`git --git-dir=${S}/.git rev-parse --verify --short origin/${branch} 2> /dev/null`
-                else
-                        head=`git --git-dir=${S}/.git rev-parse --verify --short $hash 2> /dev/null`
-                fi
-                patches=`git --git-dir=${S}/.git rev-list --count $head..HEAD 2> /dev/null`
-                printf "%s%s%s%s" +g $head +p $patches > ${S}/.scmversion
-                printf "%s%s%s%s" +g $head +p $patches > ${B}/.scmversion
-        else
+		if [ "${SRCREV}" = "INVALID" ]; then
+			hash=${SRCREV_machine}
+		else
+			hash=${SRCREV}
+		fi
+		if [ "$hash" = "AUTOINC" ]; then
+			branch=`git --git-dir=${S}/.git  symbolic-ref --short -q HEAD`
+			head=`git --git-dir=${S}/.git rev-parse --verify --short origin/${branch} 2> /dev/null`
+		else
+			head=`git --git-dir=${S}/.git rev-parse --verify --short $hash 2> /dev/null`
+		fi
+		patches=`git --git-dir=${S}/.git rev-list --count $head..HEAD 2> /dev/null`
+		printf "%s%s%s%s" +g $head +p $patches > ${S}/.scmversion
+		printf "%s%s%s%s" +g $head +p $patches > ${B}/.scmversion
+	else
 		printf "%s" "${UBOOT_LOCALVERSION}" > ${S}/.scmversion
 		printf "%s" "${UBOOT_LOCALVERSION}" > ${B}/.scmversion
 	fi