Freescale/meta-freescale

commit 4d64dde1686a017ebe2763dd7880563a6fc51b53

imx-boot: Fix 8M multi-config build problems
Building in the NXP layer meta-imx with the latest imx-boot recipe, the
dtb is not found during mkimage build:
```
| NOTE: building iMX8MP - TEE=tee.bin-stmm flash_evk_stmm_capsule
| dtc -@ -I dts -O dtb -o signature.dtbo signature.dts
| fdtoverlay -i imx8mp-evk.dtb -o imx8mp-evk.dtb signature.dtbo
| Couldn't open blob from 'imx8mp-evk.dtb': No such file or directory
```

The problem is the recipe does now copy the dtb from u-boot-imx with a
config-adorned name, e.g., imx8mp-evk.dtb-sd, but mkimage is still
expecting imx8mp-evk.dtb.

Add a link for compatibility.

Signed-off-by: Tom Hochstein <tom.hochstein@nxp.com>
(cherry picked from commit 2db7047ba40efed4d9e7c86c63d71158d974e297)

recipes-bsp/imx-mkimage/imx-boot_1.0.bb
@@ -102,17 +102,19 @@ compile_mx8m() {
 
     if [ "x${UBOOT_SIGN_ENABLE}" = "x1" ] ; then
         # Use DTB binary patched with signature node
-        cp ${DEPLOY_DIR_IMAGE}/${UBOOT_DTB_BINARY} ${BOOT_STAGING}/${UBOOT_DTB_NAME_EXTRA}
+        cp ${DEPLOY_DIR_IMAGE}/${UBOOT_DTB_BINARY}           ${BOOT_STAGING}/${UBOOT_DTB_NAME_EXTRA}
     else
-        cp ${DEPLOY_DIR_IMAGE}/${BOOT_TOOLS}/${UBOOT_DTB_NAME_EXTRA}   ${BOOT_STAGING}
+        cp ${DEPLOY_DIR_IMAGE}/${BOOT_TOOLS}/${UBOOT_DTB_NAME_EXTRA} \
+                                                             ${BOOT_STAGING}
     fi
+    ln -sf ${UBOOT_DTB_NAME_EXTRA}                           ${BOOT_STAGING}/${UBOOT_DTB_NAME}
 
     cp ${DEPLOY_DIR_IMAGE}/${BOOT_TOOLS}/u-boot-nodtb.bin-${MACHINE}-${UBOOT_CONFIG_EXTRA} \
                                                              ${BOOT_STAGING}/u-boot-nodtb.bin
 
     cp ${DEPLOY_DIR_IMAGE}/${ATF_MACHINE_NAME}               ${BOOT_STAGING}/bl31.bin
 
-    cp ${DEPLOY_DIR_IMAGE}/${UBOOT_NAME_EXTRA}                     ${BOOT_STAGING}/u-boot.bin
+    cp ${DEPLOY_DIR_IMAGE}/${UBOOT_NAME_EXTRA}               ${BOOT_STAGING}/u-boot.bin
 
 }
 

commit 61300f224008af63ebd48dea5e7e5fed64172a39

imx-boot: Fix 8M non multi-config build problem
The fix made in commit 4d64dde1686a
("imx-boot: Fix 8M multi-config build problems")
broke builds not using U-Boot multi-config, as the link created ends up being a
simple recursive link when UBOOT_DTB_NAME_EXTRA is the same as UBOOT_DTB_NAME.

It fails with something like this:
```
| ./../scripts/dtb_check.sh imx8mq-evk.dtb evk.dtb imx8mq-var-dart-dt8mcustomboard.dtb
|  Can't find u-boot DTB file, please copy from u-boot
```

caused by a symlink like this:
```
lrwxrwxrwx 1 esben 1000001 35 Oct 29 21:32 imx8mq-var-dart-dt8mcustomboard.dtb -> imx8mq-var-dart-dt8mcustomboard.dtb
```

Fixes: 4d64dde1686a ("imx-boot: Fix 8M multi-config build problems")
Signed-off-by: Esben Haabendal <esben@geanix.com>

recipes-bsp/imx-mkimage/imx-boot_1.0.bb
@@ -105,7 +105,9 @@ compile_mx8m() {
         cp ${DEPLOY_DIR_IMAGE}/${BOOT_TOOLS}/${UBOOT_DTB_NAME_EXTRA} \
                                                              ${BOOT_STAGING}
     fi
-    ln -sf ${UBOOT_DTB_NAME_EXTRA}                           ${BOOT_STAGING}/${UBOOT_DTB_NAME}
+    if [ "${UBOOT_DTB_NAME_EXTRA}" != "${UBOOT_DTB_NAME}" ] ; then
+        ln -sf ${UBOOT_DTB_NAME_EXTRA}                       ${BOOT_STAGING}/${UBOOT_DTB_NAME}
+    fi
 
     cp ${DEPLOY_DIR_IMAGE}/${BOOT_TOOLS}/u-boot-nodtb.bin-${MACHINE}-${UBOOT_CONFIG_EXTRA} \
                                                              ${BOOT_STAGING}/u-boot-nodtb.bin