Freescale/meta-freescale

commit 6bc2c1fb01f220ddebfe571389240e20ac3943f3

firmware-nxp-wifi: Added symbolic links for wifi firmware files
In the bsp LF-6.12.3 update, it is found, that the names of firmware files
were chagned. So added additional symbolic links with old file name point
to new firmware files. It is done for compatibility with linux kernel where it
references with old filename.

Signed-off-by: Sathishkumar Duraisamy <sathishkumar.d.cbe@gmail.com>

recipes-bsp/firmware-imx/firmware-nxp-wifi_1.1.bb
@@ -50,6 +50,11 @@ do_install() {
     install -d ${D}${nonarch_base_libdir}/firmware/mrvl
     ln -frs ${D}${nonarch_base_libdir}/firmware/nxp/sdiouart8997_combo_v4.bin ${D}${nonarch_base_libdir}/firmware/mrvl/sdiouart8997_combo_v4.bin
     ln -frs ${D}${nonarch_base_libdir}/firmware/nxp/sdiouartiw416_combo_v0.bin ${D}${nonarch_base_libdir}/firmware/mrvl/sdiouartiw416_combo_v0.bin
+
+    ln -frs ${D}${nonarch_base_libdir}/firmware/nxp/sd9098_wlan_v1.bin ${D}${nonarch_base_libdir}/firmware/nxp/sdio9098_wlan_v1.bin
+    ln -frs ${D}${nonarch_base_libdir}/firmware/nxp/sduart9098_combo_v1.bin ${D}${nonarch_base_libdir}/firmware/nxp/sdiouart9098_combo_v1.bin
+    ln -frs ${D}${nonarch_base_libdir}/firmware/nxp/sd8997_wlan_v4.bin ${D}${nonarch_base_libdir}/firmware/nxp/sdio8997_wlan_v4.bin
+    ln -frs ${D}${nonarch_base_libdir}/firmware/nxp/sduart8997_combo_v4.bin ${D}${nonarch_base_libdir}/firmware/nxp/sdiouart8997_combo_v4.bin
 }
 
 PACKAGES =+ " \

commit d5198caa80210b7df9555b2ca1fe16f68db106f6

firmware-nxp-wifi: Add symbolic link for IW416
Add missing symbolic link for IW416 to the name expected by the mainline
Linux kernel driver.

Fixes the following error during the driver probe:

 | mwifiex_sdio mmc2:0001:1: Direct firmware load for mrvl/sdiouartiw416_combo_v0.bin failed with error -2
 | mwifiex_sdio mmc2:0001:1: Failed to get firmware mrvl/sdiouartiw416_combo_v0.bin
 | mwifiex_sdio mmc2:0001:1: info: _mwifiex_fw_dpc: unregister device

Fixes: 6bc2c1fb01f2 ("firmware-nxp-wifi: Added symbolic links for wifi firmware files")
Signed-off-by: Francesco Dolcini <francesco.dolcini@toradex.com>

recipes-bsp/firmware-imx/firmware-nxp-wifi_1.1.bb
@@ -55,6 +55,7 @@ do_install() {
     ln -frs ${D}${nonarch_base_libdir}/firmware/nxp/sduart9098_combo_v1.bin ${D}${nonarch_base_libdir}/firmware/nxp/sdiouart9098_combo_v1.bin
     ln -frs ${D}${nonarch_base_libdir}/firmware/nxp/sd8997_wlan_v4.bin ${D}${nonarch_base_libdir}/firmware/nxp/sdio8997_wlan_v4.bin
     ln -frs ${D}${nonarch_base_libdir}/firmware/nxp/sduart8997_combo_v4.bin ${D}${nonarch_base_libdir}/firmware/nxp/sdiouart8997_combo_v4.bin
+    ln -frs ${D}${nonarch_base_libdir}/firmware/nxp/sduartiw416_combo.bin ${D}${nonarch_base_libdir}/firmware/nxp/sdiouartiw416_combo_v0.bin
 }
 
 PACKAGES =+ " \