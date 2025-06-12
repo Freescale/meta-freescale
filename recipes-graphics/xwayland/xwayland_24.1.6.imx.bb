Freescale/meta-freescale

commit 2a1583a69cf6ab11f425fbe4cfc27f0b212fa448

xwayland: 23.2.5.imx -> 24.1.6-imx
Signed-off-by: Rodrigo M. Duarte <rodrigo.duarte@ossystems.com.br>

recipes-graphics/xwayland/xwayland_24.1.6.imx.bb
@@ -1,6 +1,6 @@
 # This recipe copy helps keep meta-freescale master branch compatible
-# with Yocto langdale.
-# (From OE-Core rev: 35fdbd0ea81650a0421d50fb53989d96c5956331)
+# with Yocto walnascar.
+# (From OE-Core rev: 737e612ca36bbdf415a911644eb7592cf9389846847b47fa46dc705bd754d2d7)
 
 SUMMARY = "XWayland is an X Server that runs under Wayland."
 DESCRIPTION = "XWayland is an X Server running as a Wayland client, \
@@ -16,8 +16,8 @@ LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://COPYING;md5=5df87950af51ac2c5822094553ea1880"
 
 SRC_URI = "https://www.x.org/archive/individual/xserver/${BP_ORIGINAL}.tar.xz"
-BP_ORIGINAL = "${BPN}-23.2.5"
-SRC_URI[sha256sum] = "33ec7ff2687a59faaa52b9b09aa8caf118e7ecb6aed8953f526a625ff9f4bd90"
+BP_ORIGINAL = "${BPN}-24.1.6"
+SRC_URI[sha256sum] = "737e612ca36bbdf415a911644eb7592cf9389846847b47fa46dc705bd754d2d7"
 S = "${WORKDIR}/${BP_ORIGINAL}"
 
 UPSTREAM_CHECK_REGEX = "xwayland-(?P<pver>\d+(\.(?!90\d)\d+)+)\.tar"
@@ -28,10 +28,11 @@ REQUIRED_DISTRO_FEATURES = "x11 opengl"
 DEPENDS += "xorgproto xtrans pixman libxkbfile libxfont2 wayland wayland-native wayland-protocols libdrm libepoxy libxcvt"
 
 OPENGL_PKGCONFIGS = "glx glamor dri3"
-PACKAGECONFIG ??= "${XORG_CRYPTO} \
+PACKAGECONFIG ??= "${XORG_CRYPTO} ${XWAYLAND_EI} \
                    ${@bb.utils.contains('DISTRO_FEATURES', 'opengl', '${OPENGL_PKGCONFIGS}', '', d)} \
 "
 PACKAGECONFIG[dri3] = "-Ddri3=true,-Ddri3=false,libxshmfence"
+PACKAGECONFIG[libdecor] = "-Dlibdecor=true,-Dlibdecor=false,libdecor"
 PACKAGECONFIG[glx] = "-Dglx=true,-Dglx=false,virtual/libgl virtual/libx11"
 PACKAGECONFIG[glamor] = "-Dglamor=true,-Dglamor=false,libepoxy virtual/libgbm,libegl"
 PACKAGECONFIG[unwind] = "-Dlibunwind=true,-Dlibunwind=false,libunwind"
@@ -42,6 +43,10 @@ XORG_CRYPTO ??= "openssl"
 PACKAGECONFIG[openssl] = "-Dsha1=libcrypto,,openssl"
 PACKAGECONFIG[nettle] = "-Dsha1=libnettle,,nettle"
 PACKAGECONFIG[gcrypt] = "-Dsha1=libgcrypt,,libgcrypt"
+XWAYLAND_EI ??= "xwayland_ei_false"
+PACKAGECONFIG[xwayland_ei_false] = "-Dxwayland_ei=false"
+PACKAGECONFIG[xwayland_ei_portal] = "-Dxwayland_ei=portal,,libei"
+PACKAGECONFIG[xwayland_ei_socket] = "-Dxwayland_ei=socket,,libei"
 
 do_install:append() {
     # remove files not needed and clashing with xserver-xorg
@@ -51,3 +56,7 @@ do_install:append() {
 FILES:${PN} += "${libdir}/xorg/protocol.txt"
 
 RDEPENDS:${PN} += "xkbcomp"
+
+CVE_STATUS_GROUPS = "CVE_STATUS_REDHAT"
+CVE_STATUS_REDHAT = "CVE-2025-26594 CVE-2025-26595 CVE-2025-26596 CVE-2025-26597 CVE-2025-26598 CVE-2025-26599 CVE-2025-26600 CVE-2025-26601"
+CVE_STATUS_REDHAT[status] = "fixed-version: these are tracked as versionless redhat CVEs in NVD DB, fixed in 24.1.6"

conf/machine/include/imx-base.inc
@@ -594,7 +594,7 @@ PREFERRED_VERSION_weston:imx-mainline-bsp   = ""
 
 PREFERRED_VERSION_wayland-protocols:imx-nxp-bsp  ??= "1.37.imx"
 
-PREFERRED_VERSION_xwayland:imx-nxp-bsp ??= "23.2.5.imx"
+PREFERRED_VERSION_xwayland:imx-nxp-bsp ??= "24.1.6.imx"
 
 # Use i.MX libdrm Version
 PREFERRED_VERSION_libdrm:imx-nxp-bsp ??= "2.4.123.imx"

commit fafe47ced6672c68d8a3b0e6c5eac6fdaaa71ff9

xwayland: add missing libtirpc dependency
Current recipe is failing with the following error:

| ../xwayland-24.1.6/os/meson.build:66:8: ERROR: Problem encountered:
secure-rpc requested, but neither libtirpc or libc RPC support were
found

Fix it by adding libtirpc as dependency. This aligns with the xwayland
recipe from oe-core layer [1].

[1] https://github.com/openembedded/openembedded-core/commit/6334fac0a0b0783298957e2ccbe3a27490f7da09

Fixes: 2a1583a69cf6 ("xwayland: 23.2.5.imx -> 24.1.6-imx")
Signed-off-by: Hiago De Franco <hiago.franco@toradex.com>

recipes-graphics/xwayland/xwayland_24.1.6.imx.bb
@@ -25,7 +25,7 @@ UPSTREAM_CHECK_REGEX = "xwayland-(?P<pver>\d+(\.(?!90\d)\d+)+)\.tar"
 inherit meson features_check pkgconfig
 REQUIRED_DISTRO_FEATURES = "x11 opengl"
 
-DEPENDS += "xorgproto xtrans pixman libxkbfile libxfont2 wayland wayland-native wayland-protocols libdrm libepoxy libxcvt"
+DEPENDS += "xorgproto xtrans pixman libxkbfile libxfont2 wayland wayland-native wayland-protocols libdrm libepoxy libxcvt libtirpc"
 
 OPENGL_PKGCONFIGS = "glx glamor dri3"
 PACKAGECONFIG ??= "${XORG_CRYPTO} ${XWAYLAND_EI} \