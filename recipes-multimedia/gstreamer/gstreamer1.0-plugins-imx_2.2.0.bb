Freescale/meta-freescale

commit 25c0c7d34a6031ca1d347433291ad07c2c93626e

gst/gstreamer: Bump 1.26.0 -> 1.26.6
Signed-off-by: Zelan Zou <zelan.zou@nxp.com>

recipes-multimedia/gstreamer/gstreamer1.0-plugins-imx_2.2.0.bb
@@ -1,66 +1,0 @@
-# Copyright (C) 2018 O.S. Systems Software LTDA.
-DESCRIPTION = "GStreamer 1.0 plugins for i.MX platforms"
-LICENSE = "LGPL-2.0-or-later"
-LIC_FILES_CHKSUM = "file://LICENSE;md5=55ca817ccb7d5b5b66355690e9abc605"
-SECTION = "multimedia"
-DEPENDS = "gstreamer1.0 gstreamer1.0-plugins-base libimxdmabuffer"
-# add the audioparsers and the videoparsersbad plugins as RDEPENDS ; audioparsers
-# for the uniaudio decoder, videoparsersbad for the VPU video decoder
-# the gstreamer1.0-plugins-imx RDEPENDS is necessary to ensure the -good recipe is
-# built (it is not a compile-time dependency however, hence RDEPENDS and not DEPENDS)
-RDEPENDS:gstreamer1.0-plugins-imx = "gstreamer1.0-plugins-good gstreamer1.0-plugins-bad"
-RDEPENDS:gstreamer1.0-plugins-imx-imxaudio = "gstreamer1.0-plugins-good-audioparsers"
-RDEPENDS:gstreamer1.0-plugins-imx-imxvpu = "gstreamer1.0-plugins-bad-videoparsersbad"
-
-PV .= "+git${SRCPV}"
-
-SRCBRANCH ?= "master"
-SRCREV = "ce4f86e60f12c56574f727f3317fa8aa30a11387"
-SRC_URI = "git://github.com/Freescale/gstreamer-imx.git;branch=${SRCBRANCH};protocol=https"
-
-inherit pkgconfig meson use-imx-headers
-
-# libg2d on i.MX8 SoCs with a DPU is emulated via the DPU.
-# That particular libg2d .so depends on libdrm, however.
-# Also, due to behavioral differences, an additional flag
-# is needed to improve performance.
-LIBG2D_DPU_OPTION = "-Dg2d-based-on-dpu=false"
-LIBG2D_DEPENDENCIES = "virtual/libg2d"
-LIBG2D_DPU_OPTION:imxdpu = "-Dg2d-based-on-dpu=true"
-LIBG2D_DEPENDENCIES:imxdpu = "virtual/libg2d libdrm"
-
-# v4l2-amphion and v4l2-isi are meant for the Amphion Malone
-# VPU decoder and the ISI drivers in older kernels, which had
-# lots of bugs that required nontrivial workarounds. In the
-# kernels in this release, newer drivers are present, which
-# obviate the need for these special elements.
-EXTRA_OEMESON += "-Dimx-headers-path=${STAGING_INCDIR_IMX} -Dv4l2-amphion=disabled -Dv4l2-isi=false"
-
-PACKAGECONFIG ?= "uniaudiodec"
-PACKAGECONFIG:append:imxgpu2d = " g2d"
-PACKAGECONFIG:append:imxvpu   = " vpu"
-PACKAGECONFIG:append:imxipu   = " ipu"
-PACKAGECONFIG:append:imxpxp   = " pxp"
-# The custom imxv4l2 source and sink elements are only
-# available on the i.MX6.
-# The 2D blitter sinks require an MXC framebuffer, which
-# is not available anymore on the i.MX8 (since these SoCs
-# now use KMS instead of the old Linux framebuffer).
-PACKAGECONFIG:append:mx6-nxp-bsp      = " imx2dvideosink v4l2-mxc-source-sink"
-PACKAGECONFIG:append:mx7-nxp-bsp      = " imx2dvideosink"
-
-PACKAGECONFIG[g2d] = "-Dg2d=enabled ${LIBG2D_DPU_OPTION},-Dg2d=disabled,${LIBG2D_DEPENDENCIES}"
-PACKAGECONFIG[pxp] = "-Dpxp=enabled,-Dpxp=disabled,"
-PACKAGECONFIG[ipu] = "-Dipu=enabled,-Dipu=disabled,"
-PACKAGECONFIG[vpu] = "-Dvpu=enabled,-Dvpu=disabled,libimxvpuapi2"
-PACKAGECONFIG[imx2dvideosink] = "-Dimx2d-videosink=true,-Dimx2d-videosink=false,"
-PACKAGECONFIG[v4l2-mxc-source-sink] = "-Dv4l2-mxc-source-sink=true,-Dv4l2-mxc-source-sink=false,"
-PACKAGECONFIG[uniaudiodec] = "-Duniaudiodec=enabled,-Duniaudiodec=disabled,imx-codec"
-PACKAGECONFIG[mp3encoder] = "-Dmp3encoder=enabled,-Dmp3encoder=disabled,imx-codec"
-
-require gstreamer1.0-plugins-packaging.inc
-
-# the following line is required to produce one package for each plugin
-PACKAGES_DYNAMIC = "^${PN}-.*"
-
-COMPATIBLE_MACHINE = "(mx6dl-nxp-bsp|mx6q-nxp-bsp|mx6sl-nxp-bsp|mx6sx-nxp-bsp|mx6ul-nxp-bsp|mx6ull-nxp-bsp|mx7d-nxp-bsp|mx8-nxp-bsp)"

conf/machine/include/imx-base.inc
@@ -538,20 +538,20 @@ MACHINE_GSTREAMER_1_0_PLUGIN:mx8ulp-nxp-bsp ?= "imx-gst1.0-plugin"
 MACHINE_GSTREAMER_1_0_PLUGIN:mx93-nxp-bsp   ?= "imx-gst1.0-plugin"
 
 # GStreamer forked recipes
-PREFERRED_VERSION_gstreamer1.0:imx-nxp-bsp              ??= "1.26.0.imx"
-PREFERRED_VERSION_gstreamer1.0-plugins-bad:imx-nxp-bsp  ??= "1.26.0.imx"
-PREFERRED_VERSION_gstreamer1.0-plugins-base:imx-nxp-bsp ??= "1.26.0.imx"
-PREFERRED_VERSION_gstreamer1.0-plugins-good:imx-nxp-bsp ??= "1.26.0.imx"
+PREFERRED_VERSION_gstreamer1.0:imx-nxp-bsp              ??= "1.26.6.imx"
+PREFERRED_VERSION_gstreamer1.0-plugins-bad:imx-nxp-bsp  ??= "1.26.6.imx"
+PREFERRED_VERSION_gstreamer1.0-plugins-base:imx-nxp-bsp ??= "1.26.6.imx"
+PREFERRED_VERSION_gstreamer1.0-plugins-good:imx-nxp-bsp ??= "1.26.6.imx"
 
 
 # GStreamer copied recipes
-PREFERRED_VERSION_gst-devtools:imx-nxp-bsp              ??= "1.26.0.imx"
+PREFERRED_VERSION_gst-devtools:imx-nxp-bsp              ??= "1.26.6.imx"
 PREFERRED_VERSION_gst-examples:imx-nxp-bsp              ??= "1.24.7.imx"
-PREFERRED_VERSION_gstreamer1.0-libav:imx-nxp-bsp        ??= "1.26.0.imx"
-PREFERRED_VERSION_gstreamer1.0-plugins-ugly:imx-nxp-bsp ??= "1.26.0.imx"
-PREFERRED_VERSION_gstreamer1.0-python:imx-nxp-bsp       ??= "1.26.0.imx"
-PREFERRED_VERSION_gstreamer1.0-rtsp-server:imx-nxp-bsp  ??= "1.26.0.imx"
-PREFERRED_VERSION_gstreamer1.0-vaapi:imx-nxp-bsp        ??= "1.26.0.imx"
+PREFERRED_VERSION_gstreamer1.0-libav:imx-nxp-bsp        ??= "1.26.6.imx"
+PREFERRED_VERSION_gstreamer1.0-plugins-ugly:imx-nxp-bsp ??= "1.26.6.imx"
+PREFERRED_VERSION_gstreamer1.0-python:imx-nxp-bsp       ??= "1.26.6.imx"
+PREFERRED_VERSION_gstreamer1.0-rtsp-server:imx-nxp-bsp  ??= "1.26.6.imx"
+PREFERRED_VERSION_gstreamer1.0-vaapi:imx-nxp-bsp        ??= "1.26.6.imx"
 
 # Determines if the SoC has support for Vivante kernel driver
 SOC_HAS_VIVANTE_KERNEL_DRIVER_SUPPORT        = "0"

recipes-multimedia/gstreamer/gst-devtools_1.26.6.bb
@@ -6,13 +6,13 @@ SECTION = "multimedia"
 LICENSE = "LGPL-2.1-or-later"
 LIC_FILES_CHKSUM = "file://validate/COPYING;md5=a6f89e2100d9b6cdffcea4f398e37343"
 
-SRC_URI = "https://gstreamer.freedesktop.org/src/gst-devtools/gst-devtools-${@get_gst_ver("${PV}")}.tar.xz \
+#S = "${WORKDIR}/gst-devtools-${PV}"
+
+SRC_URI = "https://gstreamer.freedesktop.org/src/gst-devtools/gst-devtools-${PV}.tar.xz \
            file://0001-connect-has-a-different-signature-on-musl.patch \
            "
 
-S = "${UNPACKDIR}/gst-devtools-${@get_gst_ver("${PV}")}"
-
-SRC_URI[sha256sum] = "eff33d7dc292bb074a2788ea887b6283398ffdefafa49fb7d08efe6658a65648"
+SRC_URI[sha256sum] = "dec8fc56d578d65c498e65e56efe44994c5d3f4e85dbbdff0242b441b32e19b2"
 
 DEPENDS = "json-glib glib-2.0 glib-2.0-native gstreamer1.0 gstreamer1.0-plugins-base"
 RRECOMMENDS:${PN} = "git"
@@ -40,9 +40,6 @@ EXTRA_OEMESON += " \
     -Dvalidate=enabled \
     ${@gettext_oemeson(d)} \
 "
-# Drop .imx from PV
-def get_gst_ver(v):
-    return oe.utils.trim_version(v, 3)
 
 do_install:append () {
      for fn in ${bindir}/gst-validate-launcher \
@@ -53,3 +50,10 @@ do_install:append () {
 
 GIR_MESON_ENABLE_FLAG = "enabled"
 GIR_MESON_DISABLE_FLAG = "disabled"
+
+########### i.MX overrides ################
+
+EXTRA_OEMESON:append:mx93-nxp-bsp = " -Dintrospection=disabled "
+EXTRA_OEMESON:append:mx943-nxp-bsp = " -Dintrospection=disabled "
+
+########### End of i.MX overrides #########

recipes-multimedia/gstreamer/gst-examples/0001-Make-player-examples-installable.patch
@@ -1,37 +1,0 @@
-From 7924016fce2d0b435891a335cdae52fc939c7e3b Mon Sep 17 00:00:00 2001
-From: Jussi Kukkonen <jussi.kukkonen@intel.com>
-Date: Thu, 17 Aug 2017 11:07:02 +0300
-Subject: [PATCH] Make player examples installable
-
-Signed-off-by: Jussi Kukkonen <jussi.kukkonen@intel.com>
-Upstream-Status: Denied [Upstream considers these code examples, for now a least]
-
-https://bugzilla.gnome.org/show_bug.cgi?id=777827
-
----
- playback/player/gst-play/meson.build | 1 +
- playback/player/gtk/meson.build      | 1 +
- 2 files changed, 2 insertions(+)
-
-Index: gst-examples/playback/player/gst-play/meson.build
-===================================================================
---- gst-examples.orig/playback/player/gst-play/meson.build
-+++ gst-examples/playback/player/gst-play/meson.build
-@@ -2,5 +2,6 @@ executable('gst-play',
-     ['gst-play.c',
-      'gst-play-kb.c',
-      'gst-play-kb.h'],
-+    install: true,
-     dependencies : [gst_dep, dependency('gstreamer-play-1.0'), m_dep])
- 
-Index: gst-examples/playback/player/gtk/meson.build
-===================================================================
---- gst-examples.orig/playback/player/gtk/meson.build
-+++ gst-examples/playback/player/gtk/meson.build
-@@ -20,5 +20,6 @@ if gtk_dep.found()
-        'gtk-video-renderer.h',
-        'gtk-video-renderer.c'],
-       c_args :  extra_c_args,
-+      install: true,
-       dependencies : [gst_dep, gsttag_dep, gstplay_dep, gtk_dep, x11_dep])
- endif

recipes-multimedia/gstreamer/gst-examples/gst-player.desktop
@@ -1,10 +1,0 @@
-[Desktop Entry]
-Name=Media Player
-Comment=Basic media player
-Icon=video-player
-TryExec=gtk-play
-Exec=gtk-play
-StartupNotify=true
-Terminal=false
-Type=Application
-Categories=GTK;AudioVideo;

recipes-multimedia/gstreamer/gst-examples_1.24.7.imx.bb
@@ -1,38 +1,0 @@
-# This builds an older version which is compatible with the gstreamer NXP fork 1.24.7.imx,
-# thus the version is appended with '.imx'
-
-SUMMARY = "GStreamer examples (including gtk-play, gst-play)"
-DESCRIPTION = "GStreamer example applications."
-HOMEPAGE = "https://gitlab.freedesktop.org/gstreamer/gst-examples"
-BUGTRACKER = "https://gitlab.freedesktop.org/gstreamer/gst-examples/-/issues"
-LICENSE = "LGPL-2.0-or-later"
-LIC_FILES_CHKSUM = "file://playback/player/gtk/gtk-play.c;beginline=1;endline=20;md5=f8c72dae3d36823ec716a9ebcae593b9"
-
-DEPENDS = "glib-2.0 gstreamer1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-bad gtk+3 json-glib glib-2.0-native"
-
-SRC_URI = "git://gitlab.freedesktop.org/gstreamer/gstreamer.git;protocol=https;branch=1.24 \
-           file://0001-Make-player-examples-installable.patch \
-           file://gst-player.desktop \
-           "
-
-SRCREV = "0f1e984e41ce8d6d0758265d35e8b10d5815fde2"
-
-S = "${UNPACKDIR}/${BP}/subprojects/gst-examples"
-
-inherit meson pkgconfig features_check
-
-UPSTREAM_CHECK_GITTAGREGEX = "(?P<pver>\d+\.(\d*[02468])+(\.\d+)+)"
-
-ANY_OF_DISTRO_FEATURES = "${GTK3DISTROFEATURES}"
-
-do_install:append() {
-	install -m 0644 -D ${UNPACKDIR}/gst-player.desktop ${D}${datadir}/applications/gst-player.desktop
-}
-
-RDEPENDS:${PN} = "gstreamer1.0-plugins-base-playback"
-RRECOMMENDS:${PN} = "gstreamer1.0-plugins-base-meta \
-                     gstreamer1.0-plugins-good-meta \
-                     gstreamer1.0-plugins-bad-meta \
-                      ${@bb.utils.contains("LICENSE_FLAGS_ACCEPTED", "commercial", "gstreamer1.0-libav", "", d)} \
-                     ${@bb.utils.contains("LICENSE_FLAGS_ACCEPTED", "commercial", "gstreamer1.0-plugins-ugly-meta", "", d)}"
-RPROVIDES:${PN} += "gst-player gst-player-bin"

recipes-multimedia/gstreamer/gstreamer1.0-libav_1.26.6.bb
@@ -11,10 +11,10 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=69333daa044cb77e486cc36129f7a770 \
                     file://ext/libav/gstav.h;beginline=1;endline=18;md5=a752c35267d8276fd9ca3db6994fca9c \
                     "
 
-SRC_URI = "https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-${@get_gst_ver('${PV}')}.tar.xz"
-SRC_URI[sha256sum] = "707a8b687ff5fddcee5b02415e2ec9b71b4ac44d0b7aec3b477364ceecbf1ecf"
+SRC_URI = "https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-${PV}.tar.xz"
+SRC_URI[sha256sum] = "6e50a6222d509c52b19143f9a7bd3581e22c745d0c4bc27ddb07e1229bcc11b8"
 
-S = "${UNPACKDIR}/gst-libav-${@get_gst_ver('${PV}')}"
+S = "${UNPACKDIR}/gst-libav-${PV}"
 
 DEPENDS = "gstreamer1.0 gstreamer1.0-plugins-base ffmpeg"
 
@@ -24,11 +24,5 @@ EXTRA_OEMESON += " \
     -Dtests=disabled \
 "
 
-# Drop .imx from PV
-def get_gst_ver(v):
-    return oe.utils.trim_version(v, 3)
-
 FILES:${PN} += "${libdir}/gstreamer-1.0/*.so"
 FILES:${PN}-staticdev += "${libdir}/gstreamer-1.0/*.a"
-
-COMPATIBLE_MACHINE = "(imx-nxp-bsp)"

recipes-multimedia/gstreamer/gstreamer1.0-plugins-bad/0001-MMFMWK-9590-gstcuda-disable-gir-build-for-cuda-plugi.patch
@@ -1,0 +1,30 @@
+From 71b5b04685e6b03b7e2325fed22c83d9c6300f65 Mon Sep 17 00:00:00 2001
+From: Haihua Hu <jared.hu@nxp.com>
+Date: Fri, 6 Feb 2026 17:32:34 +0900
+Subject: [PATCH] MMFMWK-9590 gstcuda: disable gir build for cuda plugin
+ libraries
+
+This is a workaround to only disable cuda gir build to avoid totally
+disable bad library gir build. mesa-rvgpu-emu patch break cuda gir build
+
+Upstream-Status: Inappropriate [i.MX specific]
+---
+ gst-libs/gst/cuda/meson.build | 2 +-
+ 1 file changed, 1 insertion(+), 1 deletion(-)
+
+diff --git a/gst-libs/gst/cuda/meson.build b/gst-libs/gst/cuda/meson.build
+index 50ae0276ce..5e8a3d209b 100644
+--- a/gst-libs/gst/cuda/meson.build
++++ b/gst-libs/gst/cuda/meson.build
+@@ -157,7 +157,7 @@ gstcuda= library('gstcuda-' + api_version,
+ 
+ gen_sources = []
+ library_def = {'lib': gstcuda}
+-if build_gir
++if false
+   gir_includes = ['Gst-1.0', 'GstBase-1.0', 'GstVideo-1.0', 'CudaGst-1.0']
+   if gstglproto_dep.found()
+     gir_includes += ['GstGL-1.0']
+-- 
+2.34.1
+

commit a9a4455fa6327bc3c212379d29f8863120aa6116

gstreamer1.0-plugins-imx: Add accidentally deleted recipe
Commit 25c0c7d34a6031ca1d347433291ad07c2c93626e accidentally deleted
the recipe, so add it again to undo this.

Signed-off-by: Carlos Rafael Giani <crg7475@mailbox.org>

recipes-multimedia/gstreamer/gstreamer1.0-plugins-imx_2.2.0.bb
@@ -1,0 +1,66 @@
+# Copyright (C) 2018 O.S. Systems Software LTDA.
+DESCRIPTION = "GStreamer 1.0 plugins for i.MX platforms"
+LICENSE = "LGPL-2.0-or-later"
+LIC_FILES_CHKSUM = "file://LICENSE;md5=55ca817ccb7d5b5b66355690e9abc605"
+SECTION = "multimedia"
+DEPENDS = "gstreamer1.0 gstreamer1.0-plugins-base libimxdmabuffer"
+# add the audioparsers and the videoparsersbad plugins as RDEPENDS ; audioparsers
+# for the uniaudio decoder, videoparsersbad for the VPU video decoder
+# the gstreamer1.0-plugins-imx RDEPENDS is necessary to ensure the -good recipe is
+# built (it is not a compile-time dependency however, hence RDEPENDS and not DEPENDS)
+RDEPENDS:gstreamer1.0-plugins-imx = "gstreamer1.0-plugins-good gstreamer1.0-plugins-bad"
+RDEPENDS:gstreamer1.0-plugins-imx-imxaudio = "gstreamer1.0-plugins-good-audioparsers"
+RDEPENDS:gstreamer1.0-plugins-imx-imxvpu = "gstreamer1.0-plugins-bad-videoparsersbad"
+
+PV .= "+git${SRCPV}"
+
+SRCBRANCH ?= "master"
+SRCREV = "ce4f86e60f12c56574f727f3317fa8aa30a11387"
+SRC_URI = "git://github.com/Freescale/gstreamer-imx.git;branch=${SRCBRANCH};protocol=https"
+
+inherit pkgconfig meson use-imx-headers
+
+# libg2d on i.MX8 SoCs with a DPU is emulated via the DPU.
+# That particular libg2d .so depends on libdrm, however.
+# Also, due to behavioral differences, an additional flag
+# is needed to improve performance.
+LIBG2D_DPU_OPTION = "-Dg2d-based-on-dpu=false"
+LIBG2D_DEPENDENCIES = "virtual/libg2d"
+LIBG2D_DPU_OPTION:imxdpu = "-Dg2d-based-on-dpu=true"
+LIBG2D_DEPENDENCIES:imxdpu = "virtual/libg2d libdrm"
+
+# v4l2-amphion and v4l2-isi are meant for the Amphion Malone
+# VPU decoder and the ISI drivers in older kernels, which had
+# lots of bugs that required nontrivial workarounds. In the
+# kernels in this release, newer drivers are present, which
+# obviate the need for these special elements.
+EXTRA_OEMESON += "-Dimx-headers-path=${STAGING_INCDIR_IMX} -Dv4l2-amphion=disabled -Dv4l2-isi=false"
+
+PACKAGECONFIG ?= "uniaudiodec"
+PACKAGECONFIG:append:imxgpu2d = " g2d"
+PACKAGECONFIG:append:imxvpu   = " vpu"
+PACKAGECONFIG:append:imxipu   = " ipu"
+PACKAGECONFIG:append:imxpxp   = " pxp"
+# The custom imxv4l2 source and sink elements are only
+# available on the i.MX6.
+# The 2D blitter sinks require an MXC framebuffer, which
+# is not available anymore on the i.MX8 (since these SoCs
+# now use KMS instead of the old Linux framebuffer).
+PACKAGECONFIG:append:mx6-nxp-bsp      = " imx2dvideosink v4l2-mxc-source-sink"
+PACKAGECONFIG:append:mx7-nxp-bsp      = " imx2dvideosink"
+
+PACKAGECONFIG[g2d] = "-Dg2d=enabled ${LIBG2D_DPU_OPTION},-Dg2d=disabled,${LIBG2D_DEPENDENCIES}"
+PACKAGECONFIG[pxp] = "-Dpxp=enabled,-Dpxp=disabled,"
+PACKAGECONFIG[ipu] = "-Dipu=enabled,-Dipu=disabled,"
+PACKAGECONFIG[vpu] = "-Dvpu=enabled,-Dvpu=disabled,libimxvpuapi2"
+PACKAGECONFIG[imx2dvideosink] = "-Dimx2d-videosink=true,-Dimx2d-videosink=false,"
+PACKAGECONFIG[v4l2-mxc-source-sink] = "-Dv4l2-mxc-source-sink=true,-Dv4l2-mxc-source-sink=false,"
+PACKAGECONFIG[uniaudiodec] = "-Duniaudiodec=enabled,-Duniaudiodec=disabled,imx-codec"
+PACKAGECONFIG[mp3encoder] = "-Dmp3encoder=enabled,-Dmp3encoder=disabled,imx-codec"
+
+require recipes-multimedia/gstreamer/gstreamer1.0-plugins-packaging.inc
+
+# the following line is required to produce one package for each plugin
+PACKAGES_DYNAMIC = "^${PN}-.*"
+
+COMPATIBLE_MACHINE = "(mx6dl-nxp-bsp|mx6q-nxp-bsp|mx6sl-nxp-bsp|mx6sx-nxp-bsp|mx6ul-nxp-bsp|mx6ull-nxp-bsp|mx7d-nxp-bsp|mx8-nxp-bsp)"