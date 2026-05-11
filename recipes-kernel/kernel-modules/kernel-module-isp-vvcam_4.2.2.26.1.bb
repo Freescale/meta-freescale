Freescale/meta-freescale

commit 5594fa535392c6e0bb1e7deef646aab992591858

isp-imx: Upgrade to 4.2.2.26.1
Base on lf-6.18.2-1.0.0

Signed-off-by: Zelan Zou <zelan.zou@nxp.com>

recipes-kernel/kernel-modules/kernel-module-isp-vvcam_4.2.2.26.1.bb
@@ -1,0 +1,22 @@
+# Copyright 2020-2026 NXP
+
+DESCRIPTION = "Kernel loadable module for ISP"
+LICENSE = "GPL-2.0-only"
+LIC_FILES_CHKSUM = "file://${S}/../LICENSE;md5=64381a6ea83b48c39fe524c85f65fb44"
+
+SRC_URI = "${ISP_KERNEL_SRC};branch=${SRCBRANCH}"
+ISP_KERNEL_SRC ?= "git://github.com/nxp-imx/isp-vvcam.git;protocol=https"
+SRCBRANCH = "lf-6.18.y_1.0.0"
+SRCREV = "5484bc99a258562c3893808736dd67af22ac355e"
+
+S = "${UNPACKDIR}/${BP}/vvcam/v4l2"
+
+inherit module
+
+MODULES_MODULE_SYMVERS_LOCATION = "dwe"
+
+DEBUG_PREFIX_MAP:prepend = " \
+    -fmacro-prefix-map=${UNPACKDIR}/${BP}/vvcam=/usr/src/debug/${PN}/${EXTENDPE}${PV}-${PR} \
+    -fdebug-prefix-map=${UNPACKDIR}/${BP}/vvcam=/usr/src/debug/${PN}/${EXTENDPE}${PV}-${PR} "
+
+COMPATIBLE_MACHINE = "(mx8mp-nxp-bsp)"

recipes-bsp/isp-imx/isp-imx/0001-isp-imx-start_isp-don-t-report-error-if-no-camera-is.patch
@@ -1,32 +1,0 @@
-From d24c74528fefd1316f1633f0a7f1a5acea40454f Mon Sep 17 00:00:00 2001
-From: Max Krummenacher <max.krummenacher@toradex.com>
-Date: Thu, 19 Jan 2023 15:51:24 +0000
-Subject: [PATCH 1/5] isp-imx: start_isp: don't report error if no camera is
- configured
-
-The script currently returns '6' when no known camera is configured
-in the device tree. The end result is that the systemd imx8-isp.service
-goes to the failed state.
-Return '0' in that case as obviously the device tree doesn't have a
-camera configured and the service is not needed.
-
-Upstream-Status: Pending
-Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
----
- imx/start_isp.sh | 2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
-
-diff --git a/imx/start_isp.sh b/imx/start_isp.sh
-index d544b4ca2a57..22e7783437d6 100755
---- a/imx/start_isp.sh
-+++ b/imx/start_isp.sh
-@@ -72,5 +72,5 @@ elif [ $NR_DEVICE_TREE_OS08A20 -eq 2 ]; then
- else
- 	# no device tree found exit with code no device or address
- 	echo "No device tree found for Basler camera or os08a20, check dtb file!" >&2
--	exit 6
-+	exit 0
- fi
--- 
-2.42.0
-

recipes-bsp/isp-imx/isp-imx/0002-appshell-cmake-bump-min-version-to-3.5.patch
@@ -1,134 +1,0 @@
-From 5b69b60bd4d0d5ed1474c6457561d48cdbabc778 Mon Sep 17 00:00:00 2001
-From: Max Krummenacher <max.krummenacher@toradex.com>
-Date: Fri, 25 Jul 2025 09:19:58 +0000
-Subject: [PATCH 2/5] appshell: cmake: bump min version to 3.5
-
-CMake 4 no longer supports min version set to before 3.5
-
-|  CMake Error at CMakeLists.txt:28 (cmake_minimum_required):
-|    Compatibility with CMake < 3.5 has been removed from CMake.
-
-Upstream-Status: Pending
-Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
----
- appshell/CMakeLists.txt                        | 2 +-
- appshell/display/CMakeLists.txt                | 2 +-
- appshell/display/wayland-client/CMakeLists.txt | 2 +-
- appshell/v4l_drm_test/CMakeLists.txt           | 2 +-
- appshell/vvext/CMakeLists.txt                  | 2 +-
- appshell/yuv/CMakeLists.txt                    | 2 +-
- units/CMakeLists.txt                           | 2 +-
- units/isi/CMakeLists.txt                       | 2 +-
- units/isi/drv/OS08a20/CMakeLists.txt           | 2 +-
- units/isi/drv/OS08a20/calib/CMakeLists.txt     | 2 +-
- 10 files changed, 10 insertions(+), 10 deletions(-)
-
-diff --git a/appshell/CMakeLists.txt b/appshell/CMakeLists.txt
-index 22092ce975b5..cf14e254c812 100644
---- a/appshell/CMakeLists.txt
-+++ b/appshell/CMakeLists.txt
-@@ -25,7 +25,7 @@
- # *****************************************************************************/
- 
- 
--cmake_minimum_required(VERSION 3.1.0)
-+cmake_minimum_required(VERSION 3.5.0)
- project(ISP_APP_SHELL)
- 
- if(DEFINED QTLESS)
-diff --git a/appshell/display/CMakeLists.txt b/appshell/display/CMakeLists.txt
-index 491f086adf55..458a46b165b4 100755
---- a/appshell/display/CMakeLists.txt
-+++ b/appshell/display/CMakeLists.txt
-@@ -1,4 +1,4 @@
--cmake_minimum_required(VERSION 2.6)
-+cmake_minimum_required(VERSION 3.6)
- 
- set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -fPIC")
- 
-diff --git a/appshell/display/wayland-client/CMakeLists.txt b/appshell/display/wayland-client/CMakeLists.txt
-index 78247528b0e4..0639d79d8346 100644
---- a/appshell/display/wayland-client/CMakeLists.txt
-+++ b/appshell/display/wayland-client/CMakeLists.txt
-@@ -1,4 +1,4 @@
--cmake_minimum_required(VERSION 2.6)
-+cmake_minimum_required(VERSION 3.6)
- 
- set(WAYLAND_XML_PRIFIX ${SDKTARGETSYSROOT}/usr/share/wayland-protocols)
- set(GEN_INSTALL_PREFIX ${PROJECT_SOURCE_DIR}/display/wayland-client)
-diff --git a/appshell/v4l_drm_test/CMakeLists.txt b/appshell/v4l_drm_test/CMakeLists.txt
-index cf4685c84204..309e5d465e99 100755
---- a/appshell/v4l_drm_test/CMakeLists.txt
-+++ b/appshell/v4l_drm_test/CMakeLists.txt
-@@ -1,4 +1,4 @@
--cmake_minimum_required(VERSION 2.6)
-+cmake_minimum_required(VERSION 3.5)
- 
- project(ISP_V4L_DRM_TEST)
- set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-diff --git a/appshell/vvext/CMakeLists.txt b/appshell/vvext/CMakeLists.txt
-index a5d6c12929fa..63c1eb5f7073 100755
---- a/appshell/vvext/CMakeLists.txt
-+++ b/appshell/vvext/CMakeLists.txt
-@@ -1,4 +1,4 @@
--cmake_minimum_required(VERSION 2.6)
-+cmake_minimum_required(VERSION 3.5)
- 
- project(ISP_V4L_DRM_TEST)
- set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-diff --git a/appshell/yuv/CMakeLists.txt b/appshell/yuv/CMakeLists.txt
-index 131766fcb5d4..0f3591918e08 100755
---- a/appshell/yuv/CMakeLists.txt
-+++ b/appshell/yuv/CMakeLists.txt
-@@ -1,4 +1,4 @@
--cmake_minimum_required(VERSION 2.6)
-+cmake_minimum_required(VERSION 3.5)
- set (CMAKE_CXX_FLAGS_DEBUG "")
- set (CMAKE_C_FLAGS_DEBUG "")
- 
-diff --git a/units/CMakeLists.txt b/units/CMakeLists.txt
-index 37fa65ed4ea8..10abbef1c16c 100755
---- a/units/CMakeLists.txt
-+++ b/units/CMakeLists.txt
-@@ -24,7 +24,7 @@
- # *
- # *****************************************************************************/
- 
--cmake_minimum_required(VERSION 3.1.0)
-+cmake_minimum_required(VERSION 3.5.0)
- project(ISPcore_SW_STACK)
- if( NOT DEFINED APPSHELL_TOP_COMPILE)
- 
-diff --git a/units/isi/CMakeLists.txt b/units/isi/CMakeLists.txt
-index a91699b4a1a5..ea01aa7d9424 100755
---- a/units/isi/CMakeLists.txt
-+++ b/units/isi/CMakeLists.txt
-@@ -1,4 +1,4 @@
--cmake_minimum_required(VERSION 2.6)
-+cmake_minimum_required(VERSION 3.5)
- 
- # define module name
- set (module isi)
-diff --git a/units/isi/drv/OS08a20/CMakeLists.txt b/units/isi/drv/OS08a20/CMakeLists.txt
-index d27dc97f7945..12c6e9ef993a 100755
---- a/units/isi/drv/OS08a20/CMakeLists.txt
-+++ b/units/isi/drv/OS08a20/CMakeLists.txt
-@@ -1,4 +1,4 @@
--cmake_minimum_required(VERSION 2.6)
-+cmake_minimum_required(VERSION 3.5)
- 
- # define module name & interface version
- set (module os08a20)
-diff --git a/units/isi/drv/OS08a20/calib/CMakeLists.txt b/units/isi/drv/OS08a20/calib/CMakeLists.txt
-index b44b08248135..18119086b967 100755
---- a/units/isi/drv/OS08a20/calib/CMakeLists.txt
-+++ b/units/isi/drv/OS08a20/calib/CMakeLists.txt
-@@ -1,4 +1,4 @@
--cmake_minimum_required(VERSION 2.6)
-+cmake_minimum_required(VERSION 3.5)
- 
- # use upper level module name
- 
--- 
-2.42.0
-

recipes-bsp/isp-imx/isp-imx/0003-appshell-cmake-drop-deprecated-use-of-target_link_li.patch
@@ -1,37 +1,0 @@
-From db07010535d47d7d7eaf3865d97dcec045e19930 Mon Sep 17 00:00:00 2001
-From: Max Krummenacher <max.krummenacher@toradex.com>
-Date: Fri, 25 Jul 2025 09:57:22 +0000
-Subject: [PATCH 3/5] appshell: cmake: drop deprecated use of
- target_link_libraries
-
-Works around:
-
-| CMake Error at display/CMakeLists.txt:48 (target_link_libraries):
-|   Cannot specify link libraries for target "wayland-client" which is not
-|   built by this project.
-
-caused by CMake 4 deprecating https://cmake.org/cmake/help/v3.5/policy/CMP0016.html
-
-CMP0016 states that the command is silently ignored, thus drop it.
-
-Upstream-Status: Pending
-Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
----
- appshell/display/CMakeLists.txt | 1 -
- 1 file changed, 1 deletion(-)
-
-diff --git a/appshell/display/CMakeLists.txt b/appshell/display/CMakeLists.txt
-index 458a46b165b4..160755312368 100755
---- a/appshell/display/CMakeLists.txt
-+++ b/appshell/display/CMakeLists.txt
-@@ -43,7 +43,6 @@ if (WAYLAND_SUPPORT)
-     file(GLOB pub_headers *.h)
-     file(GLOB libsources *.cpp ./wayland-client/*.c)
-     include_directories(./wayland-client)
--    target_link_libraries(wayland-client)
- else (WAYLAND_SUPPORT)
-     file(GLOB pub_headers *.h)
-     file(GLOB libsources *.cpp)
--- 
-2.42.0
-

recipes-bsp/isp-imx/isp-imx/0004-units-targets.cmake-fix-check-if-a-target-exists.patch
@@ -1,51 +1,0 @@
-From d7e06f57c067e50d86369577b8dc3bccdde6fd40 Mon Sep 17 00:00:00 2001
-From: Max Krummenacher <max.krummenacher@toradex.com>
-Date: Fri, 25 Jul 2025 10:44:40 +0000
-Subject: [PATCH 4/5] units/targets.cmake: fix check if a target exists
-
-| CMake Error at units/targets.cmake:35 (get_target_property):
-|   get_target_property() called with non-existent target "lib".
-
-Upstream-Status: Pending
-Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
----
- units/targets.cmake | 9 +++------
- 1 file changed, 3 insertions(+), 6 deletions(-)
-
-diff --git a/units/targets.cmake b/units/targets.cmake
-index 663a024d9129..d78dbf8b8a45 100755
---- a/units/targets.cmake
-+++ b/units/targets.cmake
-@@ -32,8 +32,7 @@ add_dependencies(create_alldir ${module}_create_dir)
- # Settings for target LIB
- ###############################################################################
- # check if lib target already exists and create it, this is to assure that only one target called lib will be generated
--get_target_property(LIB_TARGET lib TYPE)
--if(NOT LIB_TARGET)
-+if(NOT TARGET lib)
-     add_custom_target(lib)
- endif()
- # the library itself is a dependency for the target lib
-@@ -83,8 +82,7 @@ if(HAVE_PUBLIC_HEADERS)
- endif()
- 
- # check if lib_install target already exists and create it
--get_target_property(LIB_INSTALL_TARGET lib_install TYPE)
--if(NOT LIB_INSTALL_TARGET)
-+if(NOT TARGET lib_install)
-     add_custom_target(lib_install)
- endif()
- 
-@@ -104,8 +102,7 @@ add_custom_target(${module}_copy_header
-                     COMMENT "Copying Headers of ${module}")
- 
- # check if copy_header target already exists and create it
--get_target_property(COPY_HEADER_TARGET copy_header TYPE)
--if(NOT COPY_HEADER_TARGET)
-+if(NOT TARGET copy_header)
-     add_custom_target(copy_header)
- endif()
- # if copy_header is selected, modulename_copy_header target must be created
--- 
-2.42.0
-

recipes-bsp/isp-imx/isp-imx/0005-units-cmake-fix-use-of-add_dependencies.patch
@@ -1,36 +1,0 @@
-From 72d6160d1efc9df8bdee4e3dc9da67560f2206b4 Mon Sep 17 00:00:00 2001
-From: Max Krummenacher <max.krummenacher@toradex.com>
-Date: Fri, 25 Jul 2025 10:52:52 +0000
-Subject: [PATCH 5/5] units: cmake: fix use of add_dependencies
-
-With CMake 4 we get during configure:
-
-| CMake Error at isp-imx/4.2.2.25.2/sources/isp-imx-4.2.2.25.2-3cac1fb:
-|   The dependency target "drv/OS08a20" of target "copy_shell_libs_isi" does
-|   not exist.
-
-Comment out that the add_dependencies. The driver for OS08a20 is still
-part of the built packages.
-
-Upstream-Status: Pending
-Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
----
- units/isi/CMakeLists.txt | 2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
-
-diff --git a/units/isi/CMakeLists.txt b/units/isi/CMakeLists.txt
-index ea01aa7d9424..2f5c3d803e67 100755
---- a/units/isi/CMakeLists.txt
-+++ b/units/isi/CMakeLists.txt
-@@ -93,7 +93,7 @@ add_custom_target(copy_shell_libs_${module} ALL
-        COMMAND ${CMAKE_COMMAND} -E copy ${UNITS_TOP_DIRECTORY}/isi/drv/*/*.cfg  ${LIB_ROOT}/${CMAKE_BUILD_TYPE}/bin/
-        COMMAND ${CMAKE_COMMAND} -E copy ${UNITS_TOP_DIRECTORY}/isi/drv/*.cfg  ${LIB_ROOT}/${CMAKE_BUILD_TYPE}/bin/
- )
--add_dependencies(copy_shell_libs_${module} drv/OS08a20)
-+#add_dependencies(copy_shell_libs_${module} os08a20_shared)
- 
- endif (GENERATE_PARTITION_BUILD)
- 
--- 
-2.42.0
-

recipes-bsp/isp-imx/isp-imx_4.2.2.26.1.bb
@@ -1,24 +1,17 @@
-# Copyright (C) 2020-2024 NXP
+# Copyright 2020-2025 NXP
 
 DESCRIPTION = "i.MX Verisilicon Software ISP"
 LICENSE = "Proprietary"
-LIC_FILES_CHKSUM = "file://COPYING;md5=a93b654673e1bc8398ed1f30e0813359"
+LIC_FILES_CHKSUM = "file://COPYING;md5=bc649096ad3928ec06a8713b8d787eac"
+
 DEPENDS = "boost libdrm virtual/libg2d libtinyxml2 jsoncpp patchelf-native"
 
-SRC_URI = " \
-    ${FSL_MIRROR}/${BP}-${IMX_SRCREV_ABBREV}.bin;fsl-eula=true \
-    ${@bb.utils.contains('DISTRO_FEATURES', 'systemd', '${ISP_SYSTEMD_PATCH}', '', d)} \
-    file://0002-appshell-cmake-bump-min-version-to-3.5.patch \
-    file://0003-appshell-cmake-drop-deprecated-use-of-target_link_li.patch \
-    file://0004-units-targets.cmake-fix-check-if-a-target-exists.patch \
-    file://0005-units-cmake-fix-use-of-add_dependencies.patch \
-"
-ISP_SYSTEMD_PATCH = "file://0001-isp-imx-start_isp-don-t-report-error-if-no-camera-is.patch"
+SRC_URI = "${FSL_MIRROR}/${BP}-${IMX_SRCREV_ABBREV}.bin;fsl-eula=true"
+SRC_URI[sha256sum] = "ab04d9eae4917591ca21f4ae13269c4e5a6f1b8e2f357cca1693682fa9a87249"
 
-IMX_SRCREV_ABBREV = "3cac1fb"
+IMX_SRCREV_ABBREV = "3cbd4a2"
+
 S = "${UNPACKDIR}/${BP}-${IMX_SRCREV_ABBREV}"
-
-SRC_URI[sha256sum] = "8fa5094da6438505287f4dcc8033dad3057ab81bf98c858884f7c3a2e521b252"
 
 inherit fsl-eula-unpack cmake pkgconfig systemd use-imx-headers
 
@@ -52,17 +45,8 @@ EXTRA_OECMAKE += " \
     -D3A_SRC_BUILD=0 \
     -DIMX_G2D=ON \
     -Wno-dev \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
 "
-
-do_configure_disable:prepend () {
-    # FIXME: should be rebuild.
-    patchelf --replace-needed libjsoncpp.so.25 libjsoncpp.so.26 ${S}/mediacontrol/install/bin/isp_media_server
-    patchelf --replace-needed libjsoncpp.so.25 libjsoncpp.so.26 ${S}/mediacontrol/install/lib/libmedia_server.so
-    patchelf --replace-needed libjsoncpp.so.25 libjsoncpp.so.26 ${S}/tuningext/install/tuningext
-
-    # FIXME: Should be rebuild.
-    patchelf --replace-needed libtinyxml2.so.10 libtinyxml2.so.11 ${S}/appshell/shell_libs/ispcore/ARM64/libcam_device.so
-}
 
 do_install() {
     # The Makefile unconditionally installs tuningext even if it is not built
@@ -80,7 +64,6 @@ do_install() {
         install -d ${D}${systemd_system_unitdir}
         install -m 0644 ${S}/imx/imx8-isp.service ${D}${systemd_system_unitdir}
     fi
-
 }
 
 # The build contains a mix of versioned and unversioned libraries, so
@@ -96,6 +79,8 @@ FILES_SOLIBS_VERSIONED = " \
     ${libdir}/libcppnetlib-uri.so \
     ${libdir}/libos08a20.so \
 "
+
+INSANE_SKIP:${PN} = "already-stripped"
 
 RDEPENDS:${PN} = "libdrm"
 

recipes-kernel/kernel-modules/kernel-module-isp-vvcam_4.2.2.25.4.bb
@@ -1,23 +1,0 @@
-# Copyright 2020-2023 NXP
-
-DESCRIPTION = "Kernel loadable module for ISP"
-LICENSE = "GPL-2.0-only"
-
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0-only;md5=801f80980d171dd6425610833a22dbe6"
-
-SRC_URI = "${ISP_KERNEL_SRC};branch=${SRCBRANCH}"
-ISP_KERNEL_SRC ?= "git://github.com/nxp-imx/isp-vvcam.git;protocol=https"
-SRCBRANCH = "lf-6.12.y_2.2.0"
-SRCREV = "ad057a15dd57e053e3596ae83b45ea96835acd3e"
-
-S = "${UNPACKDIR}/${BP}/vvcam/v4l2"
-
-inherit module
-
-MODULES_MODULE_SYMVERS_LOCATION = "dwe"
-
-DEBUG_PREFIX_MAP:prepend = " \
-    -fmacro-prefix-map=${WORKDIR}/sources=/usr/src/debug/${PN}/${EXTENDPE}${PV}-${PR} \
-    -fdebug-prefix-map=${WORKDIR}/sources=/usr/src/debug/${PN}/${EXTENDPE}${PV}-${PR} "
-
-COMPATIBLE_MACHINE = "(mx8mp-nxp-bsp)"

commit bd90d58fe926b335e3f8c08c0387573d8369f17e

kernel-module-isp-vvcam: fix build against pre-6.18 kernels
The 4.2.2.26.1 driver source is based on lf-6.18 and uses the v4l2_fh
API introduced in Linux v6.18. Older kernels still provide the older
helpers, which breaks the build due to missing file_to_v4l2_fh() and
mismatching v4l2_fh_add/del() prototypes.

Add a small compatibility wrapper for kernels older than v6.18.

Fixes: 5594fa535392 ("isp-imx: Upgrade to 4.2.2.26.1")
Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>

recipes-kernel/kernel-modules/kernel-module-isp-vvcam/0001-video-add-v4l2-fh-compat-for-pre-6.18-kernels.patch
@@ -1,0 +1,88 @@
+From 35d5a732cc4b53897a0e6188fae5e9c8fd1b97a1 Mon Sep 17 00:00:00 2001
+From: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>
+Date: Mon, 11 May 2026 16:00:30 +0000
+Subject: [PATCH] video: add v4l2_fh compat for pre-6.18 kernels
+
+Linux v6.18 added file_to_v4l2_fh() and changed v4l2_fh_add() and
+v4l2_fh_del() to take the file pointer so the helpers can manage
+file->private_data internally.
+
+The relevant upstream commits are:
+  618882c92681 ("media: Wrap file->private_data access with a helper function")
+  47f4b1acb4d5 ("media: Set file->private_data in v4l2_fh_add()")
+  277966749f46 ("media: Reset file->private_data to NULL in v4l2_fh_del()")
+
+Add a small compatibility layer for older kernels that keeps
+file->private_data management in this driver.
+
+Upstream-Status: Inappropriate
+Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>
+---
+ video/video.c | 30 +++++++++++++++++++++++++++---
+ 1 file changed, 27 insertions(+), 3 deletions(-)
+
+diff --git a/video/video.c b/video/video.c
+index bccf466b05f5..ce8c7d725150 100644
+--- a/video/video.c
++++ b/video/video.c
+@@ -69,6 +69,30 @@
+ #include "vvdefs.h"
+ #include "vvsensor.h"
+ 
++#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 18, 0)
++static inline struct v4l2_fh *viv_file_to_v4l2_fh(struct file *file)
++{
++	return file->private_data;
++}
++
++static inline void viv_v4l2_fh_add(struct v4l2_fh *fh, struct file *file)
++{
++	file->private_data = fh;
++	v4l2_fh_add(fh);
++}
++
++static inline void viv_v4l2_fh_del(struct v4l2_fh *fh, struct file *file)
++{
++	v4l2_fh_del(fh);
++	file->private_data = NULL;
++}
++
++#define file_to_v4l2_fh(file) viv_file_to_v4l2_fh(file)
++#else
++#define viv_v4l2_fh_add(fh, file) v4l2_fh_add(fh, file)
++#define viv_v4l2_fh_del(fh, file) v4l2_fh_del(fh, file)
++#endif
++
+ #define DEF_PLANE_NO    (0)
+ 
+ static struct viv_video_device *vvdev[VIDEO_NODE_NUM];
+@@ -573,7 +597,7 @@ static int video_open(struct file *file)
+ 	handle = kzalloc(sizeof(*handle), GFP_KERNEL);
+ 
+ 	v4l2_fh_init(&handle->vfh, dev->video);
+-	v4l2_fh_add(&handle->vfh, file);
++	viv_v4l2_fh_add(&handle->vfh, file);
+ 
+ 	handle->vdev = dev;
+ 	handle->queue.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+@@ -589,7 +613,7 @@ static int video_open(struct file *file)
+ 	rc = vb2_queue_init(&handle->queue);
+ 	if (rc) {
+ 		pr_err("can't init vb queue\n");
+-		v4l2_fh_del(&handle->vfh, file);
++		viv_v4l2_fh_del(&handle->vfh, file);
+ 		v4l2_fh_exit(&handle->vfh);
+ 		kfree(handle);
+ 		return rc;
+@@ -654,7 +678,7 @@ static int video_close(struct file *file)
+ 				vb2_buffer_done(vb, VB2_BUF_STATE_ERROR);
+ 		}
+ 
+-		v4l2_fh_del(&handle->vfh, file);
++		viv_v4l2_fh_del(&handle->vfh, file);
+ 		v4l2_fh_exit(&handle->vfh);
+ 
+ 		{
+-- 
+2.43.0
+

recipes-kernel/kernel-modules/kernel-module-isp-vvcam_4.2.2.26.1.bb
@@ -4,7 +4,9 @@ DESCRIPTION = "Kernel loadable module for ISP"
 LICENSE = "GPL-2.0-only"
 LIC_FILES_CHKSUM = "file://${S}/../LICENSE;md5=64381a6ea83b48c39fe524c85f65fb44"
 
-SRC_URI = "${ISP_KERNEL_SRC};branch=${SRCBRANCH}"
+SRC_URI = "${ISP_KERNEL_SRC};branch=${SRCBRANCH} \
+           file://0001-video-add-v4l2-fh-compat-for-pre-6.18-kernels.patch \
+"
 ISP_KERNEL_SRC ?= "git://github.com/nxp-imx/isp-vvcam.git;protocol=https"
 SRCBRANCH = "lf-6.18.y_1.0.0"
 SRCREV = "5484bc99a258562c3893808736dd67af22ac355e"