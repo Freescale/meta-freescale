Freescale/meta-freescale

commit 48dfc5f5f33ca45ebb313cf821c988885656ad12

wayland-protocols: use upstream repo with NXP downstream patches
OE-core update wayland-utils to 1.3.0 which now requires
wayland-protocols version >= 1.44.
As the imx-nxp-bsp forked wayland protocols and does not provide
a version later than 1.41 the build fails in do_configure of
wayland-utils.

Drop the wayland-protocol recipe of the downstream fork, extract
the 3 relevant patches on top of upstream v1.41 and apply them
in a bbappend on top of whatever OE-core provides for wayland-protocols.
(6 downstream patches not relevant for the build are dropped)

| ../sources/wayland-utils-1.3.0/wayland-info/meson.build:4:9:
|   ERROR: Dependency lookup for wayland-protocols with method 'pkgconfig' failed:
|     Invalid version, need 'wayland-protocols' ['>= 1.44'] found '1.41'.

The `inherit allarch` from OE-core can not easily be overridden. I.e.
just setting `PACKAGE_ARCH:imx-nxp-bsp = "${MACHINE_SOCARCH}"` was
ignored and the packages were still allarch without the workaround.

Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

wayland-protocol: drop patches 0004-0009

recipes-graphics/wayland/wayland-protocols_%.bbappend
@@ -1,0 +1,16 @@
+FILESEXTRAPATHS:prepend := "${THISDIR}/${BPN}:"
+
+NXP_PATCHES = " \
+    file://0001-unstable-Add-alpha-compositing-protocol.patch \
+    file://0002-unstable-Add-hdr10-metadata-protocol.patch \
+    file://0003-linux-dmabuf-support-passing-buffer-DTRC-meta-to-com.patch \
+"
+
+SRC_URI:append:imx-nxp-bsp = " ${NXP_PATCHES}"
+
+# override the effect of "inherit allarch"
+python allarch_package_arch_handler:prepend:imx-nxp-bsp () {
+    return
+}
+
+PACKAGE_ARCH:imx-nxp-bsp = "${MACHINE_SOCARCH}"

conf/machine/include/imx-base.inc
@@ -588,8 +588,6 @@ PREFERRED_VERSION_weston:mx6-nxp-bsp      ??= "10.0.5.imx"
 PREFERRED_VERSION_weston:mx7-nxp-bsp      ??= "10.0.5.imx"
 PREFERRED_VERSION_weston:imx-mainline-bsp   = ""
 
-PREFERRED_VERSION_wayland-protocols:imx-nxp-bsp  ??= "1.41.imx"
-
 PREFERRED_VERSION_xwayland:imx-nxp-bsp ??= "24.1.6.imx"
 
 # Use i.MX libdrm Version

recipes-graphics/wayland/wayland-protocols/0001-unstable-Add-alpha-compositing-protocol.patch
@@ -1,0 +1,199 @@
+From 50e14f75e9f42e7574a7f094d2e357124db9a588 Mon Sep 17 00:00:00 2001
+From: Haihua Hu <jared.hu@nxp.com>
+Date: Thu, 14 Jun 2018 13:54:08 +0800
+Subject: [PATCH 1/9] unstable: Add alpha-compositing protocol
+
+It's based on the Chromium Wayland protocol of the same name ([1])
+and Alexandros's modified version ([2])with a few changes
+made to the blending_equation enumeration.
+
+Add one more blending_equation "src_alpha / src_alpha"
+
+[1] https://chromium.googlesource.com/chromium/src/+/master/third_party/wayland-protocols/unstable/alpha-compositing/alpha-compositing-unstable-v1.xml
+[2] https://lists.freedesktop.org/archives/wayland-devel/2017-August/034741.html
+
+Upstream-Status: Pending
+Signed-off-by: Haihua Hu <jared.hu@nxp.com>
+
+Conflicts:
+	Makefile.am
+(cherry picked from commit ac5837440d587c88e7979b034ef54893dc45633f)
+(cherry picked from commit a521913cedf5d5f84a162c08768c52bfb841dc23)
+---
+ meson.build                                   |   1 +
+ unstable/alpha-compositing/README             |   6 +
+ .../alpha-compositing-unstable-v1.xml         | 136 ++++++++++++++++++
+ 3 files changed, 143 insertions(+)
+ create mode 100644 unstable/alpha-compositing/README
+ create mode 100644 unstable/alpha-compositing/alpha-compositing-unstable-v1.xml
+
+diff --git a/meson.build b/meson.build
+index a78d698a730b..99a555d53a09 100644
+--- a/meson.build
++++ b/meson.build
+@@ -42,6 +42,7 @@ unstable_protocols = {
+ 	'xdg-output': ['v1'],
+ 	'xdg-shell': ['v5', 'v6'],
+ 	'xwayland-keyboard-grab': ['v1'],
++	'alpha-compositing': ['v1'],
+ }
+ 
+ staging_protocols = {
+diff --git a/unstable/alpha-compositing/README b/unstable/alpha-compositing/README
+new file mode 100644
+index 000000000000..d87415682961
+--- /dev/null
++++ b/unstable/alpha-compositing/README
+@@ -0,0 +1,6 @@
++Alpha compositing protocol
++
++Maintainers:
++David Reveman <reveman at chromium.org>
++Alexandros Frantzis <alexandros.frantzis at collabora.com>
++Jared Hu <Jared Hu at nxp.com>
+diff --git a/unstable/alpha-compositing/alpha-compositing-unstable-v1.xml b/unstable/alpha-compositing/alpha-compositing-unstable-v1.xml
+new file mode 100644
+index 000000000000..0844b3fd4ecf
+--- /dev/null
++++ b/unstable/alpha-compositing/alpha-compositing-unstable-v1.xml
+@@ -0,0 +1,136 @@
++<?xml version="1.0" encoding="UTF-8"?>
++<protocol name="alpha_compositing_unstable_v1">
++
++  <copyright>
++    Copyright 2016 The Chromium Authors.
++    Copyright 2017 Collabora Ltd
++    Copyright 2018 NXP
++
++    Permission is hereby granted, free of charge, to any person obtaining a
++    copy of this software and associated documentation files (the "Software"),
++    to deal in the Software without restriction, including without limitation
++    the rights to use, copy, modify, merge, publish, distribute, sublicense,
++    and/or sell copies of the Software, and to permit persons to whom the
++    Software is furnished to do so, subject to the following conditions:
++
++    The above copyright notice and this permission notice (including the next
++    paragraph) shall be included in all copies or substantial portions of the
++    Software.
++
++    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
++    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
++    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
++    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
++    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
++    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
++    DEALINGS IN THE SOFTWARE.
++  </copyright>
++
++  <description summary="Protocol for more advanced compositing and blending">
++    This protocol specifies a set of interfaces used to control the alpha
++    compositing and blending of surface contents.
++
++    Warning! The protocol described in this file is experimental and backward
++    incompatible changes may be made. Backward compatible changes may be added
++    together with the corresponding interface version bump. Backward
++    incompatible changes are done by bumping the version number in the protocol
++    and interface names and resetting the interface version. Once the protocol
++    is to be declared stable, the 'z' prefix and the version number in the
++    protocol and interface names are removed and the interface version number is
++    reset.
++  </description>
++
++  <interface name="zwp_alpha_compositing_v1" version="1">
++    <description summary="alpha_compositing">
++      The global interface exposing compositing and blending capabilities is
++      used to instantiate an interface extension for a wl_surface object.
++      This extended interface will then allow the client to specify the
++      blending equation and alpha value used for compositing the wl_surface.
++    </description>
++
++    <request name="destroy" type="destructor">
++      <description summary="unbind from the blending interface">
++        Informs the server that the client will not be using this
++        protocol object anymore. This does not affect any other objects,
++        blending objects included.
++      </description>
++    </request>
++
++    <enum name="error">
++      <entry name="blending_exists" value="0"
++             summary="the surface already has a blending object associated"/>
++    </enum>
++
++    <request name="get_blending">
++      <description summary="extend surface interface for blending">
++        Instantiate an interface extension for the given wl_surface to
++        provide surface blending. If the given wl_surface already has
++        a blending object associated, the blending_exists protocol error
++        is raised.
++      </description>
++
++      <arg name="id" type="new_id" interface="zwp_blending_v1"
++           summary="the new blending interface id"/>
++      <arg name="surface" type="object" interface="wl_surface"
++           summary="the surface"/>
++    </request>
++  </interface>
++
++  <interface name="zwp_blending_v1" version="1">
++    <description summary="blending interface to a wl_surface">
++      An additional interface to a wl_surface object, which allows the
++      client to specify the blending equation used for compositing and
++      an alpha value applied to the whole surface.
++
++      When the blending object is created its blending equation is
++      'none' and its alpha is 1.0, i.e., it's inactive by default. Clients
++      can activate it by setting the blending equation and alpha value.
++
++      If the wl_surface associated with the blending object is destroyed,
++      the blending object becomes inert.
++
++      If the blending object is destroyed, the blending state is removed
++      from the wl_surface. The change will be applied on the next
++      wl_surface.commit.
++    </description>
++
++    <request name="destroy" type="destructor">
++      <description summary="remove blending from the surface">
++        The associated wl_surface's blending state is removed.
++        The change is applied on the next wl_surface.commit.
++      </description>
++    </request>
++
++    <enum name="blending_equation">
++      <description summary="different blending equations for compositing">
++        Blending equations that can be used when compositing a surface.
++      </description>
++      <entry name="none" value="0" summary="blending object is inactive"/>
++      <entry name="opaque" value="1" summary="(one, zero)"/>
++      <entry name="premultiplied" value="2" summary="(one, one_minus_src_alpha)"/>
++      <entry name="straight" value="3" summary="(src_alpha, one_minus_src_alpha)" />
++      <entry name="fromsource" value="4" summary="(src_alpha, src_alpha)" />
++    </enum>
++
++    <request name="set_blending">
++      <description summary="set the blending equation">
++        Set the blending equation for compositing the wl_surface.
++
++        The blending equation state is double-buffered state,
++        and will be applied on the next wl_surface.commit.
++      </description>
++      <arg name="equation" type="uint" summary="the new blending equation"/>
++    </request>
++
++    <request name="set_alpha">
++      <description summary="set the alpha value">
++        Set the alpha value applied to the whole surface for compositing.
++
++        The alpha value state is double-buffered state,
++        and will be applied on the next wl_surface.commit.
++      </description>
++      <arg name="value" type="fixed" summary="the new alpha value"/>
++    </request>
++  </interface>
++
++</protocol>
+\ No newline at end of file
+-- 
+2.42.0
+

recipes-graphics/wayland/wayland-protocols/0002-unstable-Add-hdr10-metadata-protocol.patch
@@ -1,0 +1,135 @@
+From b514647ccdd4e833b2d8fbba1a39547b80657987 Mon Sep 17 00:00:00 2001
+From: Haihua Hu <jared.hu@nxp.com>
+Date: Tue, 26 Jun 2018 15:30:18 +0800
+Subject: [PATCH 2/9] unstable: Add hdr10-metadata protocol
+
+this protocol is used to support hdr10 video playback.
+HDR10 metadata is passed to compositor via this interface
+
+Upstream-Status: Pending
+Signed-off-by: Haihua Hu <jared.hu@nxp.com>
+(cherry picked from commit 87ea413b40701fbb3d333264a1de3b168276bbcf)
+(cherry picked from commit 878f46b324fcefe5193d66c025c4c67bff09c253)
+---
+ meson.build                                   |  1 +
+ unstable/hdr10-metadata/README                |  4 +
+ .../hdr10-metadata-unstable-v1.xml            | 84 +++++++++++++++++++
+ 3 files changed, 89 insertions(+)
+ create mode 100644 unstable/hdr10-metadata/README
+ create mode 100644 unstable/hdr10-metadata/hdr10-metadata-unstable-v1.xml
+
+diff --git a/meson.build b/meson.build
+index 99a555d53a09..70b594d3b89c 100644
+--- a/meson.build
++++ b/meson.build
+@@ -43,6 +43,7 @@ unstable_protocols = {
+ 	'xdg-shell': ['v5', 'v6'],
+ 	'xwayland-keyboard-grab': ['v1'],
+ 	'alpha-compositing': ['v1'],
++	'hdr10-metadata': ['v1'],
+ }
+ 
+ staging_protocols = {
+diff --git a/unstable/hdr10-metadata/README b/unstable/hdr10-metadata/README
+new file mode 100644
+index 000000000000..58a82f802d12
+--- /dev/null
++++ b/unstable/hdr10-metadata/README
+@@ -0,0 +1,4 @@
++hdr10 metadata protocol
++
++Maintainers:
++Jared Hu <Jared Hu at nxp.com>
+diff --git a/unstable/hdr10-metadata/hdr10-metadata-unstable-v1.xml b/unstable/hdr10-metadata/hdr10-metadata-unstable-v1.xml
+new file mode 100644
+index 000000000000..5b1bac4aa29f
+--- /dev/null
++++ b/unstable/hdr10-metadata/hdr10-metadata-unstable-v1.xml
+@@ -0,0 +1,84 @@
++<?xml version="1.0" encoding="UTF-8"?>
++<protocol name="hdr10_metadata_unstable_v1">
++
++  <copyright>
++    Copyright 2018 NXP
++
++    Permission is hereby granted, free of charge, to any person obtaining a
++    copy of this software and associated documentation files (the "Software"),
++    to deal in the Software without restriction, including without limitation
++    the rights to use, copy, modify, merge, publish, distribute, sublicense,
++    and/or sell copies of the Software, and to permit persons to whom the
++    Software is furnished to do so, subject to the following conditions:
++
++    The above copyright notice and this permission notice (including the next
++    paragraph) shall be included in all copies or substantial portions of the
++    Software.
++
++    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
++    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
++    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
++    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
++    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
++    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
++    DEALINGS IN THE SOFTWARE.
++  </copyright>
++
++  <description summary="Protocol for sending hdr10 metadata to compositor">
++    This protocol specifies a set of interfaces used to set and control
++    hdr10 metadata of video contents.
++
++    Warning! The protocol described in this file is experimental and backward
++    incompatible changes may be made. Backward compatible changes may be added
++    together with the corresponding interface version bump. Backward
++    incompatible changes are done by bumping the version number in the protocol
++    and interface names and resetting the interface version. Once the protocol
++    is to be declared stable, the 'z' prefix and the version number in the
++    protocol and interface names are removed and the interface version number is
++    reset.
++  </description>
++
++  <interface name="zwp_hdr10_metadata_v1" version="1">
++    <description summary="hdr10_metadata">
++      The global interface exposing hdr10 metadata capabilities is
++      used to instantiate an interface extension for compositor.
++      This extended interface will then allow the client to pass hdr10 metadata
++      to compositor and send to display driver if supported.
++    </description>
++
++    <request name="destroy" type="destructor">
++      <description summary="unbind from the hdr10 metadata interface">
++        Informs the server that the client will not be using this
++        protocol object anymore. This does not affect any other objects.
++      </description>
++    </request>
++
++    <enum name="error">
++      <entry name="sending_meta_error" value="0"
++             summary="error occurs when compositor handle metadata"/>
++    </enum>
++
++    <request name="set_metadata">
++      <description summary="interface of setting and controling hdr10 metadata">
++        client can use this interface pass hdr10 metadata to server. Server need
++        use the recived metadata to enable hdr mode.
++      </description>
++
++      <arg name="eotf" type="uint" summary="supported eotf"/>
++      <arg name="type" type="uint" summary="supported type"/>
++      <arg name="display_primaries_red" type="uint"
++        summary="display primaries of red, high 16bits for x, and low 16bits for y"/>
++      <arg name="display_primaries_green" type="uint"
++        summary="display primaries of green, high 16bits for x, and low 16bits for y"/>
++      <arg name="display_primaries_blue" type="uint"
++        summary="display primaries of blue, high 16bits for x, and low 16bits for y"/>
++      <arg name="white_point" type="uint"
++        summary="white point, high 16bits for x, and low 16bits for blue"/>
++      <arg name="mastering_display_luminance" type="uint"
++        summary="max and min mastering display luminance, high 16 bits for max, and low 16 bits for min"/>
++      <arg name="max_cll" type="uint" summary="max content light level"/>
++      <arg name="max_fall" type="uint" summary="max frame average light level"/>
++    </request>
++  </interface>
++
++</protocol>
+-- 
+2.42.0
+

recipes-graphics/wayland/wayland-protocols/0003-linux-dmabuf-support-passing-buffer-DTRC-meta-to-com.patch
@@ -1,0 +1,53 @@
+From fd0b31e5f7d1785464cc4b83371efc6222437725 Mon Sep 17 00:00:00 2001
+From: Haihua Hu <jared.hu@nxp.com>
+Date: Wed, 5 Sep 2018 13:00:47 +0800
+Subject: [PATCH 3/9] linux-dmabuf: support passing buffer DTRC meta to
+ compositor
+
+DTRC meta is untilized to pass offset to DTRC in drm dcss driver
+to enable video tile compress
+
+Upstream-Status: Pending
+Signed-off-by: Haihua Hu <jared.hu@nxp.com>
+(cherry picked from commit 0f71ffe59cf4c3f050ff79ea18f96345fcf0061e)
+(cherry picked from commit bc76403e005be9162a09fe5bd3bdcd6b00cba7eb)
+---
+ .../linux-dmabuf/linux-dmabuf-unstable-v1.xml | 22 +++++++++++++++++++
+ 1 file changed, 22 insertions(+)
+
+diff --git a/unstable/linux-dmabuf/linux-dmabuf-unstable-v1.xml b/unstable/linux-dmabuf/linux-dmabuf-unstable-v1.xml
+index 6f11e925b68e..48d7ea003835 100644
+--- a/unstable/linux-dmabuf/linux-dmabuf-unstable-v1.xml
++++ b/unstable/linux-dmabuf/linux-dmabuf-unstable-v1.xml
+@@ -398,6 +398,28 @@
+       <arg name="format" type="uint" summary="DRM_FORMAT code"/>
+       <arg name="flags" type="uint" enum="flags" summary="see enum flags"/>
+     </request>
++
++    <request name="add_dtrc_meta">
++      <description summary="add dtrc meta in this dmabuf">
++        This request adds one dmabuf to the set in this
++        zwp_linux_buffer_params_v1.
++
++        The 64-bit unsigned value combined from modifier_hi and modifier_lo
++        is the dmabuf layout modifier. DRM AddFB2 ioctl calls this the
++        fb modifier, which is defined in drm_mode.h of Linux UAPI.
++        This is an opaque token. Drivers use this token to express tiling,
++        compression, etc. driver-specific modifications to the base format
++        defined by the DRM fourcc code.
++
++        This request raises the PLANE_IDX error if plane_idx is too large.
++        The error PLANE_SET is raised if attempting to set a plane that
++        was already set.
++      </description>
++      <arg name="rfc_chroma_offset" type="uint"
++           summary="high 32 bits of DTRC offset"/>
++      <arg name="rfc_luma_offset" type="uint"
++           summary="low 32 bits of DTRC offset"/>
++    </request>
+   </interface>
+ 
+   <interface name="zwp_linux_dmabuf_feedback_v1" version="5">
+-- 
+2.42.0
+

recipes-graphics/wayland/wayland-protocols_1.41.imx.bb
@@ -1,31 +1,0 @@
-SUMMARY = "Collection of additional Wayland protocols"
-DESCRIPTION = "Wayland protocols that add functionality not \
-available in the Wayland core protocol. Such protocols either add \
-completely new functionality, or extend the functionality of some other \
-protocol either in Wayland core, or some other protocol in \
-wayland-protocols."
-HOMEPAGE = "http://wayland.freedesktop.org"
-LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://LICENSE;md5=c7b12b6702da38ca028ace54aae3d484 \
-                    file://stable/presentation-time/presentation-time.xml;endline=26;md5=4646cd7d9edc9fa55db941f2d3a7dc53"
-
-SRC_URI = "${WAYLAND_PROTOCOLS_SRC};branch=${SRCBRANCH}"
-WAYLAND_PROTOCOLS_SRC ?= "git://github.com/nxp-imx/wayland-protocols-imx.git;protocol=https"
-SRCBRANCH = "wayland-protocols-imx-1.41"
-SRCREV = "7213a4e09837c77112bce2d241491600e00dd095"
-
-UPSTREAM_CHECK_URI = "https://gitlab.freedesktop.org/wayland/wayland-protocols/-/tags"
-UPSTREAM_CHECK_REGEX = "releases/(?P<pver>.+)"
-
-DEPENDS += "wayland-native"
-
-# NOTE: For i.MX drop allarch since the recipe is SOCARCH
-#inherit meson pkgconfig allarch
-inherit meson pkgconfig
-
-EXTRA_OEMESON += "-Dtests=false"
-
-BBCLASSEXTEND = "native nativesdk"
-
-PACKAGE_ARCH = "${MACHINE_SOCARCH}"
-COMPATIBLE_MACHINE = "(mx6-nxp-bsp|mx7-nxp-bsp|mx8-nxp-bsp|mx9-nxp-bsp)"

commit f4bb3c0f3c522a92d3ffe92c4fab7d37f9f2fa99

wayland-protocols: fix non target builds
It was found that at least in some configurations a nativesdk
(and likely a native) build failed as the wayland-protocols
artefacts weren't found.

Don't apply the patches or change the PACKAGE_ARCH if not building
for target.

See also meta-freescale issue #2525.

Fixes: 48dfc5f5f33c ("wayland-protocols: use upstream repo with NXP downstream patches")
Signed-off-by: Max Krummenacher <max.oss.09@gmail.com>

recipes-graphics/wayland/wayland-protocols_%.bbappend
@@ -6,11 +6,11 @@ NXP_PATCHES = " \
     file://0003-linux-dmabuf-support-passing-buffer-DTRC-meta-to-com.patch \
 "
 
-SRC_URI:append:imx-nxp-bsp = " ${NXP_PATCHES}"
+SRC_URI:append:imx-nxp-bsp:class-target = " ${NXP_PATCHES}"
 
 # override the effect of "inherit allarch"
-python allarch_package_arch_handler:prepend:imx-nxp-bsp () {
+python allarch_package_arch_handler:prepend:imx-nxp-bsp:class-target () {
     return
 }
 
-PACKAGE_ARCH:imx-nxp-bsp = "${MACHINE_SOCARCH}"
+PACKAGE_ARCH:imx-nxp-bsp:class-target = "${MACHINE_SOCARCH}"