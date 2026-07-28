Freescale/meta-freescale

commit 843ac09e88a27395badc54bff5ffa4a0c6180f59

gst/gstreamer: Bump 1.26.6 -> 1.28.1
Update license file reference: COPYING -> LICENSE.txt
gstreamer1.0-vaapi remains at 1.26.6

Signed-off-by: Flora Hu <flora.hu@nxp.com>

recipes-multimedia/gstreamer/gstreamer1.0-plugins-bad_1.28.1.imx.bb
@@ -189,8 +189,8 @@ SRC_URI:append:mx93-nxp-bsp = " file://0001-MMFMWK-9590-gstcuda-disable-gir-buil
 SRC_URI:append:mx943-nxp-bsp = " file://0001-MMFMWK-9590-gstcuda-disable-gir-build-for-cuda-plugi.patch"
 
 GST1.0-PLUGINS-BAD_SRC ?= "gitsm://github.com/nxp-imx/gst-plugins-bad.git;protocol=https"
-SRCBRANCH = "MM_04.10.03_2512_L6.18.2"
-SRCREV = "81b6888efe936153b88c32b22a12d60bf9395054"
+SRCBRANCH = "MM_04.11.00_2605_L6.18.20"
+SRCREV = "ae93b57a6a238604c305ca79e25d6248b5bab5d6"
 
 inherit use-imx-headers
 

conf/machine/include/imx-base.inc
@@ -529,19 +529,19 @@ MACHINE_GSTREAMER_1_0_PLUGIN:mx8ulp-nxp-bsp ?= "imx-gst1.0-plugin"
 MACHINE_GSTREAMER_1_0_PLUGIN:mx93-nxp-bsp   ?= "imx-gst1.0-plugin"
 
 # GStreamer forked recipes
-PREFERRED_VERSION_gstreamer1.0:imx-nxp-bsp              ??= "1.26.6.imx"
-PREFERRED_VERSION_gstreamer1.0-plugins-bad:imx-nxp-bsp  ??= "1.26.6.imx"
-PREFERRED_VERSION_gstreamer1.0-plugins-base:imx-nxp-bsp ??= "1.26.6.imx"
-PREFERRED_VERSION_gstreamer1.0-plugins-good:imx-nxp-bsp ??= "1.26.6.imx"
+PREFERRED_VERSION_gstreamer1.0:imx-nxp-bsp              ??= "1.28.1.imx"
+PREFERRED_VERSION_gstreamer1.0-plugins-bad:imx-nxp-bsp  ??= "1.28.1.imx"
+PREFERRED_VERSION_gstreamer1.0-plugins-base:imx-nxp-bsp ??= "1.28.1.imx"
+PREFERRED_VERSION_gstreamer1.0-plugins-good:imx-nxp-bsp ??= "1.28.1.imx"
 
 
 # GStreamer copied recipes
-PREFERRED_VERSION_gst-devtools:imx-nxp-bsp              ??= "1.26.6"
-PREFERRED_VERSION_gst-examples:imx-nxp-bsp              ??= "1.26.6"
-PREFERRED_VERSION_gstreamer1.0-libav:imx-nxp-bsp        ??= "1.26.6"
-PREFERRED_VERSION_gstreamer1.0-plugins-ugly:imx-nxp-bsp ??= "1.26.6"
-PREFERRED_VERSION_gstreamer1.0-python:imx-nxp-bsp       ??= "1.26.6"
-PREFERRED_VERSION_gstreamer1.0-rtsp-server:imx-nxp-bsp  ??= "1.26.6"
+PREFERRED_VERSION_gst-devtools:imx-nxp-bsp              ??= "1.28.1"
+PREFERRED_VERSION_gst-examples:imx-nxp-bsp              ??= "1.28.1"
+PREFERRED_VERSION_gstreamer1.0-libav:imx-nxp-bsp        ??= "1.28.1"
+PREFERRED_VERSION_gstreamer1.0-plugins-ugly:imx-nxp-bsp ??= "1.28.1"
+PREFERRED_VERSION_gstreamer1.0-python:imx-nxp-bsp       ??= "1.28.1"
+PREFERRED_VERSION_gstreamer1.0-rtsp-server:imx-nxp-bsp  ??= "1.28.1"
 PREFERRED_VERSION_gstreamer1.0-vaapi:imx-nxp-bsp        ??= "1.26.6"
 
 # Use libcamera fork for certain SOCs

recipes-multimedia/gstreamer/gst-devtools_1.28.1.bb
@@ -12,7 +12,7 @@ SRC_URI = "https://gstreamer.freedesktop.org/src/gst-devtools/gst-devtools-${PV}
            file://0001-connect-has-a-different-signature-on-musl.patch \
            "
 
-SRC_URI[sha256sum] = "dec8fc56d578d65c498e65e56efe44994c5d3f4e85dbbdff0242b441b32e19b2"
+SRC_URI[sha256sum] = "a4e49cd082972a132ca5f54be52a3c386db37c4cb0e487e017ba00d83a5f985d"
 
 DEPENDS = "glib-2.0 glib-2.0-native gstreamer1.0 gstreamer1.0-plugins-base json-glib"
 RRECOMMENDS:${PN} = "git"

recipes-multimedia/gstreamer/gst-examples_1.28.1.bb
@@ -10,12 +10,12 @@ LIC_FILES_CHKSUM = "file://playback/player/gtk/gtk-play.c;beginline=1;endline=20
 
 DEPENDS = "glib-2.0 glib-2.0-native gstreamer1.0 gstreamer1.0-plugins-bad gstreamer1.0-plugins-base gtk+3 json-glib"
 
-SRC_URI = "git://gitlab.freedesktop.org/gstreamer/gstreamer.git;protocol=https;branch=1.26 \
+SRC_URI = "git://gitlab.freedesktop.org/gstreamer/gstreamer.git;protocol=https;branch=1.28;tag=${PV} \
            file://0001-Make-player-examples-installable.patch \
            file://gst-player.desktop \
            "
 
-SRCREV = "6cbdcfaca66dd52cdc4670d6f5122c68b9161170"
+SRCREV = "dcb37e20147e3b59344bab1e1cbb57e908cc6b92"
 
 S = "${UNPACKDIR}/${BP}/subprojects/gst-examples"
 

recipes-multimedia/gstreamer/gstreamer1.0-libav_1.28.1.bb
@@ -12,7 +12,7 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=69333daa044cb77e486cc36129f7a770 \
                     "
 
 SRC_URI = "https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-${PV}.tar.xz"
-SRC_URI[sha256sum] = "6e50a6222d509c52b19143f9a7bd3581e22c745d0c4bc27ddb07e1229bcc11b8"
+SRC_URI[sha256sum] = "bfa91aaca38d0fd8addcdd559e35b7541e3f32a5f410194ec4ba18040defee9b"
 
 S = "${UNPACKDIR}/gst-libav-${PV}"
 

recipes-multimedia/gstreamer/gstreamer1.0-plugins-base_1.28.1.imx.bb
@@ -119,8 +119,8 @@ SRC_URI:append:mx93-nxp-bsp = " file://0001-MMFMWK-9590-gstgl-1.0-disable-gir-bu
 SRC_URI:append:mx943-nxp-bsp = " file://0001-MMFMWK-9590-gstgl-1.0-disable-gir-build-for-gl-plugi.patch"
 
 GST1.0-PLUGINS-BASE_SRC ?= "gitsm://github.com/nxp-imx/gst-plugins-base.git;protocol=https"
-SRCBRANCH = "MM_04.10.03_2512_L6.18.2"
-SRCREV = "48718c61610e3628f3f162bb8d84ae213ae8516f"
+SRCBRANCH = "MM_04.11.00_2605_L6.18.20"
+SRCREV = "500aeb1b0dc50198005bd60459a38b42c7359d1d"
 
 inherit use-imx-headers
 

recipes-multimedia/gstreamer/gstreamer1.0-plugins-good_1.28.1.imx.bb
@@ -115,8 +115,8 @@ SRC_URI:remove = "https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plu
 
 SRC_URI:prepend = "${GST1.0-PLUGINS-GOOD_SRC};branch=${SRCBRANCH} "
 GST1.0-PLUGINS-GOOD_SRC ?= "gitsm://github.com/nxp-imx/gst-plugins-good.git;protocol=https"
-SRCBRANCH = "MM_04.10.03_2512_L6.18.2"
-SRCREV = "447aebbd3c2b12a72f00316d60d7c86600a330f6"
+SRCBRANCH = "MM_04.11.00_2605_L6.18.20"
+SRCREV = "59dc51ce285f65dfde65c4484eb1da8b51c18841"
 
 S = "${UNPACKDIR}/${BP}"
 

commit efb060e060778a4d58e1102e53f5a34c9185701c

gstreamer1.0-plugins-bad: drop stale tinycompress PACKAGECONFIG
The NXP 1.28.1.imx source revision used by this recipe no longer
provides a Meson option named 'tinycompress', nor does it contain the
tinycompresssink plugin directory in sys/.

This can lead to the following build error:
| ../sources/gstreamer1.0-plugins-bad-1.28.1.imx/meson.build:1:0: ERROR: Unknown option: "tinycompress".

Drop the stale PACKAGECONFIG entry and the mx8 append. If
tinycompresssink is needed again, the plugin and its Meson option need
to be restored in the source tree first.

Fixes: 843ac09e88a2 ("gst/gstreamer: Bump 1.26.6 -> 1.28.1")
Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>

recipes-multimedia/gstreamer/gstreamer1.0-plugins-bad_1.28.1.imx.bb
@@ -204,14 +204,13 @@ PACKAGECONFIG_REMOVE ?= "\
 PACKAGECONFIG:remove = "${PACKAGECONFIG_REMOVE}"
 PACKAGECONFIG:remove:mx93-nxp-bsp = "gl"
 PACKAGECONFIG:remove:mx943-nxp-bsp = "gl"
-PACKAGECONFIG:append:mx8-nxp-bsp = " kms tinycompress"
+PACKAGECONFIG:append:mx8-nxp-bsp = " kms"
 
 PACKAGECONFIG:append = " ${PACKAGECONFIG_G2D}"
 PACKAGECONFIG_G2D ??= ""
 PACKAGECONFIG_G2D:imxgpu2d ??= "g2d"
 
 PACKAGECONFIG[g2d] = ",,virtual/libg2d"
-PACKAGECONFIG[tinycompress] = "-Dtinycompress=enabled,-Dtinycompress=disabled,tinycompress"
 
 EXTRA_OEMESON += "\
     -Dc_args="${CFLAGS} -I${STAGING_INCDIR_IMX}" \